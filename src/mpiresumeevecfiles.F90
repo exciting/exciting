3f12d4a028267b9a9aa75f75e1b9d8d7be986126
Christian Meisenbichler <christian.meisenbichler@unileoben.ac.at>
2007-10-05 16:29:53 +0200

    resumefile made fit for local io A

--- src/mpiresumeevecfiles.F90
+++ src/mpiresumeevecfiles.F90
@@ -27,98 +27,102 @@ subroutine    mpiresumeevecfiles
   complex(8) :: evecsv (nstsv,nstsv)
   real(8):: evalfv(nstfv,nspnfv),vkl_(3),evalsvp(nstsv),occsvp(nstsv)
   character(256), external:: outfilenamestring
+  if(splitfile.and.(nproc.gt.1))
   if(procs.gt.1) call MPI_barrier(MPI_COMM_WORLD,ierr)
-  if(procs.gt.1.and.splittfile)CALL SYSTEM("sync")
-  if ((rank.eq.0).and.(procs.gt.1).and.splittfile)  then
-     filetag='EVECFV' 
-     inquire(iolength=recl) vkl_,nmatmax_,nstfv_,nspnfv_,evecfv
-     open(71,file=trim(scrpath)//trim(filetag)//trim(filext)  ,action='WRITE', &
-          form='UNFORMATTED',access='DIRECT',recl=recl)
-     do proc=0,procs-1
-        open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
-             form='UNFORMATTED',access='DIRECT',recl=recl)
-        do ik =firstk(proc),lastk(proc)
-           read(77,rec=ik-firstk(procofk(ik))+1) vkl_,nmatmax_,nstfv_,nspnfv_,evecfv
-           write(71,rec=ik) vkl_,nmatmax_,nstfv_,nspnfv_,evecfv
-        end do
-        close(77,status='DELETE')
-     end do
-     close(71) 
-
-     filetag='EVECSV'
-     inquire(iolength=recl) vkl_,nstsv_,evecsv
-     open(71,file=trim(scrpath)//trim(filetag)//trim(filext)  ,action='WRITE', &
-          form='UNFORMATTED',access='DIRECT',recl=recl)
-     do proc=0,procs-1
-        open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
-             form='UNFORMATTED',access='DIRECT',recl=recl)
-	do ik =firstk(proc),lastk(proc)
-           read(77,rec=ik-firstk(procofk(ik))+1)  vkl_,nstsv_,evecsv
-           write(71,rec=ik)  vkl_,nstsv_,evecsv
-        end do
-        close(77,status='DELETE')
-     end do
-     close(71) 
+  if(rank.ne.0)call mpi_recv(token,1,MPI_INTEGER,MPI_COMM_WORLD,ierr)
 
 
 
-     filetag='EVALFV'
-     inquire(iolength=recl)  vkl_,nstfv_,nspnfv_,evalfv
-     open(71,file=trim(scrpath)//trim(filetag)//trim(filext)  ,action='WRITE', &
-          form='UNFORMATTED',access='DIRECT',recl=recl)
-     do proc=0,procs-1
-        open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
-             form='UNFORMATTED',access='DIRECT',recl=recl)
-	do ik =firstk(proc),lastk(proc)
-           read(77,rec=ik-firstk(procofk(ik))+1)   vkl_,nstfv_,nspnfv_,evalfv
-           write(71,rec=ik)   vkl_,nstfv_,nspnfv_,evalfv
-        end do
-        close(77,status='DELETE')
-     end do
-     close(71)
+  filetag='EVECFV' 
+  inquire(iolength=recl) vkl_,nmatmax_,nstfv_,nspnfv_,evecfv
+  open(71,file=trim(filetag)//trim(filext)  ,action='WRITE', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  proc=rank
+  open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  do ik =firstk(proc),lastk(proc)
+     read(77,rec=ik-firstk(procofk(ik))+1) vkl_,nmatmax_,nstfv_,nspnfv_,evecfv
+     write(71,rec=ik) vkl_,nmatmax_,nstfv_,nspnfv_,evecfv
+  end do
+  close(77,status='DELETE')
+
+  close(71) 
+
+  filetag='EVECSV'
+  inquire(iolength=recl) vkl_,nstsv_,evecsv
+  open(71,file=trim(filetag)//trim(filext)  ,action='WRITE', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  proc=rank
+  open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  do ik =firstk(proc),lastk(proc)
+     read(77,rec=ik-firstk(procofk(ik))+1)  vkl_,nstsv_,evecsv
+     write(71,rec=ik)  vkl_,nstsv_,evecsv
+  end do
+  close(77,status='DELETE')
+
+  close(71) 
 
 
-     filetag='EVALSV'
-     inquire(iolength=recl)  vkl_,nstsv_,evalsvp
-     open(71,file=trim(scrpath)//trim(filetag)//trim(filext)  ,action='WRITE', &
-          form='UNFORMATTED',access='DIRECT',recl=recl)
-     do proc=0,procs-1
-        open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
-             form='UNFORMATTED',access='DIRECT',recl=recl)
-	do ik =firstk(proc),lastk(proc)
-           read(77,rec=ik-firstk(procofk(ik))+1)   vkl_,nstsv_,evalsvp
-           write(71,rec=ik)   vkl_,nstsv_,evalsvp
-        end do
-        close(77,status='DELETE')
-     end do
-     close(71)
+
+  filetag='EVALFV'
+  inquire(iolength=recl)  vkl_,nstfv_,nspnfv_,evalfv
+  open(71,file=trim(filetag)//trim(filext)  ,action='WRITE', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  proc=rank
+  open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  do ik =firstk(proc),lastk(proc)
+     read(77,rec=ik-firstk(procofk(ik))+1)   vkl_,nstfv_,nspnfv_,evalfv
+     write(71,rec=ik)   vkl_,nstfv_,nspnfv_,evalfv
+  end do
+  close(77,status='DELETE')
+
+  close(71)
 
 
-     filetag='OCCSV'
-     inquire(iolength=recl)  vkl_,nstsv_,occsvp
-     open(71,file=trim(scrpath)//trim(filetag)//trim(filext)  ,action='WRITE', &
-          form='UNFORMATTED',access='DIRECT',recl=recl)
-     do proc=0,procs-1
-        open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
-             form='UNFORMATTED',access='DIRECT',recl=recl)
-	do ik =firstk(proc),lastk(proc)
-           read(77,rec=ik-firstk(procofk(ik))+1)   vkl_,nstsv_,occsvp
-           write(71,rec=ik)  vkl_,nstsv_,occsvp
-        end do
-        close(77,status='DELETE')
-     end do
-     close(71)
-     CALL SYSTEM("sync")
-     write(60,*)"resumed splitt files"
-     call flushifc(60)
-  endif
+  filetag='EVALSV'
+  inquire(iolength=recl)  vkl_,nstsv_,evalsvp
+  open(71,file=trim(filetag)//trim(filext)  ,action='WRITE', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  proc=rank
+  open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  do ik =firstk(proc),lastk(proc)
+     read(77,rec=ik-firstk(procofk(ik))+1)   vkl_,nstsv_,evalsvp
+     write(71,rec=ik)   vkl_,nstsv_,evalsvp
+  end do
+  close(77,status='DELETE')
 
-  if(procs.gt.1) call MPI_barrier(MPI_COMM_WORLD,ierr)
-  if(procs.gt.1) CALL SYSTEM("sync")
- 
-  if(procs.gt.1)call MPI_barrier(MPI_COMM_WORLD,ierr)
+  close(71)
+
+
+  filetag='OCCSV'
+  inquire(iolength=recl)  vkl_,nstsv_,occsvp
+  open(71,file=trim(filetag)//trim(filext)  ,action='WRITE', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  proc=rank
+  open(77,file=outfilenamestring(filetag,firstk(proc)),action='READ', &
+       form='UNFORMATTED',access='DIRECT',recl=recl)
+  do ik =firstk(proc),lastk(proc)
+     read(77,rec=ik-firstk(procofk(ik))+1)   vkl_,nstsv_,occsvp
+     write(71,rec=ik)  vkl_,nstsv_,occsvp
+  end do
+  close(77,status='DELETE')
+
+  close(71)
+  CALL SYSTEM("sync")
+  write(60,*)"resumed splitt files"
+  call flushifc(60)
+
+  if(rank.ne.(nproc-1))call mpi_send(token,1,MPI_INTEGER,MPI_COMM_WORLD,ierr)
+endif
+if(procs.gt.1) call MPI_barrier(MPI_COMM_WORLD,ierr)
+CALL SYSTEM("sync")
+
+
 #endif
- splittfile=.false.
-  return
+splitfile=false
+return
 end subroutine mpiresumeevecfiles
 
