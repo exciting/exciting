8fb15e93929e1a1b6c552f801bc66f154c0e2a87
Christian Meisenbichler <ChristianMeisenbichler@unileoben.ac.at>
2007-11-07 09:55:15 +0100

    fixes allocation Bug but ARPACK does converge to very wrong eigenvalues too small and not real

--- src/src_iterative_solver/iterativearpacksequn.F90
+++ src/src_iterative_solver/iterativearpacksequn.F90
@@ -11,15 +11,8 @@ subroutine iterativearpacksecequn(ik,ispn,apwalm,vgpc,evalfv,evecfv)
   !   evalfv : first-variational eigenvalues (out,real(nstfv))
   !   evecfv : first-variational eigenvectors (out,complex(nmatmax,nstfv))
   ! !DESCRIPTION:
-  ! This routine will perform several Bock Davidson iterations following the sceme:
-  ! 1. for each of m bands:
-  !   a. calculate Residual
-  !$$
-  !\ket{\mathbf{R}\left(\ket{\mathbf{A}^{ap}},E^{ap}\right)}=(\mathbf{H}-E^{ap}\mathbf{S})\ket{ \mathbf{A}^{ap}}
-  !$$
-  !   b. calculate $\delta \mathbf{A}$
-  ! 2. solve Projected system in evecsv+$\delta \mathbf{A}$ subspace
-  !EOP
+  ! This routine will perform several ARPACK iterations 
+  
   !BOC
   implicit none
   ! arguments
@@ -38,7 +31,8 @@ subroutine iterativearpacksecequn(ik,ispn,apwalm,vgpc,evalfv,evecfv)
   parameter         (zero = (0.0D+0, 0.0D+0) ,one = (1.0D+0, 0.0D+0) )
   !ARPACK Interface vars
 
-  integer:: ido, nev, ncv, lworkl, info, ierr, j,i
+  integer:: ido, nev, ncv, lworkl, info,infoznaupd, ierr, j,i
+  integer :: nevmax, ncvmax,nmax
   integer:: nconv, maxitr, ishfts, mode, ldv
   integer::iparam(11), ipntr(14)
   complex(8),allocatable::workd(:),resid(:),v(:,:),workev(:),workl(:),d(:)
@@ -52,72 +46,103 @@ subroutine iterativearpacksecequn(ik,ispn,apwalm,vgpc,evalfv,evecfv)
   integer ::IPIV(nmat(ik,ispn))
   !parameters
   nev=nstfv
-  ncv=nstfv+2
+  ncv=3*nstfv+2
+  nevmax=max(15,nstfv)
+  ncvmax= nevmax*3
+  nmax=nmatmax
   n=nmat(ik,ispn)
-ldv=n
-  allocate(workd(3*n),resid(n),v(ldv,ncv),workev(2*ncv),workl(3*ncv*ncv+5*ncv),d(ncv))
-  allocate(rwork(n))
+  ldv=n
+  allocate(workd(3*nmax),resid(nmax),v(ldv,ncvmax),workev(2*ncvmax),workl(3*ncvmax*ncvmax+6*ncvmax),d(ncvmax))
+  allocate(rwork(ncvmax))
   bmat  = 'G'
-  which = 'LM'
+  which = 'SM'
   sigma = zero
-  lworkl =3*ncv+5*ncv 
-  tol    = 0.0 
+  lworkl =3*ncvmax*ncvmax+5*ncvmax 
+  tol    = 0.0
   ido    = 0
   info   = 0
   ishfts = 1
-  maxitr = 300
+  maxitr = 2000
   mode   = 2
   iparam(1) = ishfts
   iparam(3) = maxitr  
   iparam(7) = mode 
-  call getevecfv(vkl(1,ik),vgkl(1,1,ik,1),evecfv)
-  call getevalfv(vkl(1,ik),evalfv)
+  if(iscl.ne.1) then
+     call getevecfv(vkl(1,ik),vgkl(1,1,ik,1),evecfv)
+     call getevalfv(vkl(1,ik),evalfv)
+  endif
   call hamiltonandoverlapsetup(npmat(ik,ispn),ngk(ik,ispn),apwalm,igkig(1,ik,ispn),vgpc,h,o)
-  !calculate Lu decomposition to be used in the reverse communication loop
-  call zhptrf('U', N, o, IPIV, info )
-
+  !calculate LU decomposition to be used in the reverse communication loop
+#ifdef DEBUG
+write (333,*)"h",h,"o",o
+#endif
+  call zhptrf('U', n, o, IPIV, info )
+  if (info.ne.0)then
+     write(*,*)"error in iterativearpacksecequn zhptrf ",info
+     stop
+  endif
   !################################################
   !# reverse comunication loop of arpack library: #
   !################################################
   do i=1,maxitr 
-     call znaupd(ido,bmat,n,which,nstfv,tol,resid,ncv,v,nmat(ik,ispn),\
-     iparam,ipntr,workd,workl,lworkl,rwork,info)
+     call znaupd  &
+          ( ido, bmat, n, which, nev, tol, resid, ncv, v, ldv, iparam,  &
+          ipntr, workd, workl, lworkl, rwork, infoznaupd)
+#ifdef DEBUG
+     ! write(*,*) "ido",ido
+#endif
+
      if (ido .eq. -1 .or. ido .eq. 1) then
 
 	call zhpmv("U",n,dcmplx(1.0,0.0),h,workd(ipntr(1)), 1,&
              dcmplx(0,0),workd(ipntr(2)), 1)
         call zhptrs('U', N, 1, o, IPIV, workd(ipntr(2)), n, INFO )
-        cycle
+        if (info.ne.0)then
+           write(*,*)"error in iterativearpacksecequn zhptrs ",info
+           stop
+        endif
      else if (ido .eq. 2) then
  	call zhpmv("U",n,dcmplx(1.0,0.0),o,workd(ipntr(1)), 1,&
              dcmplx(0,0),workd(ipntr(2)), 1)
-        cycle
      else 
         exit
      endif
   end do
-  if ( info .ne. 0 ) then
+  if ( infoznaupd .ne. 0 ) then
      print *, ' ' 
-     print *, ' Error with znaupd, info = ', info
+     print *, ' Error with znaupd, info = ', infoznaupd
      print *, ' Check the documentation of znaupd'
      print *, ' '
-
+     stop
   else
-     rvec = .true.
+     rvec = .false.
 
      call zneupd  (rvec,'A',select,d,v,n,sigma,&
           workev,bmat,n,which,nev,tol,resid,ncv,v,&
           n, iparam, ipntr, workd, workl, lworkl, rwork,&
           ierr )
+     !         (rvec , howmny, select, d     ,
+     !    &                   z    , ldz   , sigma , workev,
+     !    &                   bmat , n     , which , nev   ,
+     !    &                   tol  , resid , ncv   , v     ,
+     !    &                   ldv  , iparam, ipntr , workd ,
+     !    &                   workl, lworkl, rwork , info  )
      if ( ierr .ne. 0 ) then
         print *, ' ' 
         print *, ' Error with zneupd, info = ', ierr
         print *, ' Check the documentation of zneupd'
-        print *, ' '
-
+        print *, ' '	 
+	write(*,*)"eval",d(1:nstfv)
+        write(*,*)"iter",i	
+        stop
      endif
 
   endif
+#ifdef DEBUG
+  write(*,*)"eval",d(1:nstfv)	
+  write(*,*)"iterations",i
+#endif
+
   evecfv(:,1:nstfv,ispn)=v(:,1:nstfv)
   evalfv(1:nstfv,ispn)=d(1:nstfv)
   call putevecfv(ik,evecfv)
