7e89a2e6dd8f4fa73e9e1c3fdd225ff43a9a8509
Stephan Sagmeister <stephan.sagmeister@gmail.com>
2007-11-22 22:50:37 +0100

    libbzint/kgen working for symmetries properly (with debug info)

--- src/src_libbzint/reduk.f90
+++ src/src_libbzint/reduk.f90
@@ -23,6 +23,8 @@ subroutine reduk(nsymt,divsh,weight)
   implicit none
   !<sag>
   logical, allocatable :: done(:)
+  integer :: iv(3)
+  real(8) :: kpr(3),kprt(3)
   !</sag>
 
 
@@ -147,21 +149,31 @@ write(*,'(a,3i6,i9)') 'libbzint: knr,kpid',i1,i2,i3,kpid
                  !              write(24,'(i6,3i4)')divsh,div
                  !              write(24,'(3i4,"  ",3i4)')onek,kk
                  do i=1,nsymt
-                    do j=1,3
-                       nkp(j)=mod(iio(j,1,i)*kk(1)+iio(j,2,i)*kk(2)+&
-                            &                 iio(j,3,i)*kk(3),divsh*div(j))
-                       ktp(j)=nkp(j)
-                       nkp(j)=nkp(j)+(1-isign(1,nkp(j)))*divsh*div(j)/2
-                       nkp(j)=(nkp(j)-shift(j))/divsh
-                    enddo
+                    ! from internal coordinates to lattice coordinates
+                    kpr(:)=dble(kk(:))/dble(divsh*div(:))
+                    ! rotate k-point
+                    kprt(:)=iio(:,1,i)*kpr(1)+iio(:,2,i)*kpr(2)+&
+                         iio(:,3,i)*kpr(3)
+                    ! map to reciprocal unit cell
+                    call r3frac(1.d-6,kprt,iv)
+                    ! from lattice coordinates to internal coordinates
+                    kprt(:)=kprt(:)*divsh*div(:)
+                    nkp(:)=nint(kprt(:))
+                    ! determine fractional part of rotated k-point
+                    call r3frac(1.d-6,kprt,iv)
+                    ! if fractional part is present discard k-point
+                    if (any(kprt>1.d-6)) nkp(:)=-1
+
                     !               write(24,'(i6,3i4,"  ",3i4)')i,ktp,nkp
                     nkpid=idkp(nkp)
                     !                write(24,'(4x,2i4)')nirkp,nkpid
                     !---                call jset(kpav,nkpid,1)
-                    done(nkpid)=.true. !+++
-                    redkp(nkpid)=kpid
-                    starr(i)=nkpid
-!write(*,'(a,i9,3i6,i9)') '  sym,nkp,nkpid',i,nkp,nkpid
+                    if (.not.all(nkp.eq.-1)) then
+                       done(nkpid)=.true. !+++
+                       redkp(nkpid)=kpid
+                       starr(i)=nkpid
+                    end if
+write(1234,'(a,2i9,3i6,i9)') '  kpid,sym,nkp,nkpid',kpid,i,nkp,nkpid
                  enddo
                  members=0
                  do i=1,nsymt
@@ -191,5 +203,34 @@ write(*,'(a,3i6,i9)') 'libbzint: knr,kpid',i1,i2,i3,kpid
   !      write(6,*)'               reduk: end'
   !      write(6,*)'------------------------------------------------------'
   return
+contains
+subroutine r3frac(eps,v,iv)
+! Copyright (C) 2002-2005 J. K. Dewhurst, S. Sharma and C. Ambrosch-Draxl.
+! This file is distributed under the terms of the GNU Lesser General Public
+! License.
+implicit none
+! arguments
+real(8), intent(in) :: eps
+real(8), intent(inout) :: v(3)
+integer, intent(out) :: iv(3)
+! local variables
+integer i
+do i=1,3
+  iv(i)=int(v(i))
+  v(i)=v(i)-dble(iv(i))
+  if (v(i).lt.0.d0) then
+    v(i)=v(i)+1.d0
+    iv(i)=iv(i)-1
+  end if
+  if (1.d0-v(i).lt.eps) then
+    v(i)=0.d0
+    iv(i)=iv(i)+1
+  end if
+  if (v(i).lt.eps) then
+    v(i)=0.d0
+  end if
+end do
+return
+end subroutine
 end subroutine reduk
 !EOC
