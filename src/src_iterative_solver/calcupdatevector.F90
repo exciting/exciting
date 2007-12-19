01a63a7340e9d682dfe63642844714391f533ec8
Christian Meisenbichler <ChristianMeisenbichler@unileoben.ac.at>
2007-12-19 14:47:10 +0100

    calcupdate gives reasonable vector^

--- src/src_iterative_solver/calcupdatevector.F90
+++ src/src_iterative_solver/calcupdatevector.F90
@@ -8,27 +8,29 @@ subroutine calcupdatevectors(n,iunconverged,P,w,r,evalfv,evecfv,phi)
   complex(8),intent(out)::phi(n,nstfv)
   real(8), intent(in)::w(nmatmax),evalfv(nstfv)
   complex(8)::z,v(n,nstfv)
-  integer m,i
+  integer m,i,j
   m=nstfv
-  !call zgemm('C','N',n,m,n,zone,P,nmatmax,r,n,zzero,v,n)
-  
-  do i=1,m
-   call zgemv('C',n,n,zone,P,nmatmax,r(1,i),1,zzero,v(1,i),1)
-     if(abs(w(i)-evalfv(i)).lt.1e-2)then
-        z= cmplx(1.0/(w(i)-evalfv(i)),0)
-        call zscal(n,z,v(1,i),1)
-        write(*,*)"hier z",z
-       call zgemv('N',n,n,zone,P,nmatmax,v(1,i),1,zzero,v(1,i),1) 
-     else
-      v(:,i)=0
-     endif
+  call zgemm('C','N',n,m,n,zone,P,nmatmax,r,n,zzero,v,n)
+
+  do j=1,m
+     do i=1,n
+        z= w(i)-evalfv(j)
+        if(dble(z).lt.1e-2)then  
+           v(i,j)=v(i,j)/z
+        else
+           v(i,j)=0
+        endif
+     end do
+
   end do
+
   do i=1,m
-  	call zcopy(n,evecfv(1,i),1,phi(1,i),1)
+     call zcopy(n,evecfv(1,i),1,phi(1,i),1)
   end do
-   write(771,*),phi(:,2)
+
+  write(771,*),phi(:,2)
 
   call zgemm('N','N',n,m,n,zone,P,nmatmax,v,n,zone,phi,n)
- 
-   write(772,*),phi(:,2) 
+
+  write(772,*),phi(:,2) 
 end subroutine calcupdatevectors
