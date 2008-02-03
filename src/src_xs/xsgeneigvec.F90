a4a06a9648d73c2c390f7879943ca0883b7391e7
Stephan Sagmeister <stephan.sagmeister@gmail.com>
2008-02-04 00:39:45 +0100

    small change

---
+++ src/src_xs/xsgeneigvec.F90
@@ -0,0 +1,99 @@
+
+! Copyright (C) 2004-2007 S. Sagmeister and C. Ambrosch-Draxl.
+! This file is distributed under the terms of the GNU General Public License.
+! See the file COPYING for license details.
+
+subroutine xsgeneigvec
+  use modmain
+  use modxs
+  use modmpi
+  use m_gndstateq
+  use m_genapwcmt
+  use m_getunit
+  use m_writegqpts
+  use m_filedel
+  use m_genfilname
+  implicit none
+  ! local variables
+  character(*), parameter :: thisnam='xsgeneigvec'
+  real(8), parameter :: zero3(3)=0.d0
+  integer :: iq,qi,qf
+  integer :: ik,recl
+  ! initialize universal variables
+  call init0
+  call init1
+  call init2xs
+  ! SCF allready parallelized for k-point set
+  qi=1
+  ! add extra q-point for if files for q=0 are to be calculated
+  if (tq0ev) qi=0
+  qf=nqpt
+  ! write q-points
+  if (rank.eq.0) call writeqpts
+  ! allocate arrays for APW expansion coefficients
+  allocate(apwdlm(nstsv,apwordmax,lmmaxapwtd,natmtot))
+  ! read from STATE.OUT exclusively
+  isreadstate0=.true.
+  ! calculate eigenvectors for each q-point (k+q point set)
+  do iq=qi,qf
+     ! file extension for q-point
+     call genfilname(iqmt=max(0,iq),setfilext=.true.)
+     ! one more iteration for q=0
+     if (iq.eq.0) then
+        call gndstateq(vkloff,filext)
+        write(unitout,'(a)') 'Info('//thisnam//'): eigenvectors generated &
+             &for Gamma q-point'
+     else
+        ! write G+q vectors
+        call writegqpts(iq)
+        ! shift k-mesh with q-point
+        call gndstateq(qvkloff(:,iq),filext)
+        write(unitout,'(a,i8)') 'Info('//thisnam//'): eigenvectors generated &
+             &for q-point:', iq
+     end if
+     ! store product of eigenvectors with matching coefficients
+     if (allocated(evecfv)) deallocate(evecfv)
+     allocate(evecfv(nmatmax,nstfv,nspnfv))
+     if (allocated(apwalm)) deallocate(apwalm)
+     allocate(apwalm(ngkmax,apwordmax,lmmaxapw,natmtot))
+     call getunit(unit1)
+     inquire(iolength=recl) vql(:,1),vkl(:,1),apwdlm
+     open(unit1,file='APWDLM'//trim(filext),action='write',&
+          form='unformatted',status='replace',access='direct',recl=recl)
+     do ik=1,nkpt
+        call getevecfv(vkl(1,ik),vgkl(1,1,ik,1),evecfv)
+        call match(ngk(ik,1),gkc(1,ik,1),tpgkc(1,1,ik,1),sfacgk(1,1,ik,1), &
+             apwalm)
+        call genapwcmt(lmaxapwtd,ngk(ik,1),1,nstsv,apwalm,evecfv,apwdlm)
+        if (iq.eq.0) then
+           write(unit1,rec=ik) zero3,vkl(:,ik),apwdlm
+        else
+           write(unit1,rec=ik) vql(:,max(iq,1)),vkl(:,ik),apwdlm
+        end if
+     end do
+     close(unit1)
+     if (rank.eq.0) then
+        ! safely remove unnecessary files
+        call filedel('EQATOMS'//trim(filext))
+        call filedel('EVALCORE'//trim(filext))
+        call filedel('FERMIDOS'//trim(filext))
+        call filedel('GEOMETRY'//trim(filext))
+        call filedel('LATTICE'//trim(filext))
+        call filedel('IADIST'//trim(filext))
+        call filedel('LINENGY'//trim(filext))
+        call filedel('SYMCRYS'//trim(filext))
+        call filedel('SYMLAT'//trim(filext))
+        call filedel('SYMSITE'//trim(filext))
+        call filedel('TOTENERGY'//trim(filext))
+        call filedel('EVALFV'//trim(filext))
+        call filedel('RMSDVEFF'//trim(filext))
+     end if
+     ! end loop over q-points
+  end do
+  isreadstate0=.false.
+  deallocate(apwdlm,evecfv)
+  call barrier
+  write(unitout,'(a)') "Info("//trim(thisnam)//"): generation of &
+       &eigenvectors finished"
+  call genfilname(setfilext=.true.)
+end subroutine xsgeneigvec
