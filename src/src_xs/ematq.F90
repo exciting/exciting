57011cb5311861c5d432e09c9185bd363c806932
Stephan Sagmeister <stephan.sagmeister@gmail.com>
2008-02-04 15:02:02 +0100

    changes in ematq

--- src/src_xs/ematq.F90
+++ src/src_xs/ematq.F90
@@ -34,6 +34,58 @@ subroutine ematq(iq)
   call ematbdlims(1,nst1,istlo1,isthi1,nst2,istlo2,isthi2)
   ! generate radial integrals wrt. sph. Bessel functions
   call ematrad(iq)
+  ! delete timing information of previous runs
+  call filedel(trim(fnetim))
+  ! write information
+  write(unitout,'(a,i6)') 'Info('//thisnam//'): number of G+q vectors:', &
+       ngq(iq)
+  call ematqalloc
+  ! loop over k-points
+  do ik=kpari,kparf
+     call ematqk1(iq,ik)
+     ! synchronize for common number of k-points to all processes
+     if ((partype.eq.'k').and.(ik-kpari+1 <= nkpt/procs)) call barrier
+     ! end loop over k-points
+  end do
+  call ematqdealloc
+end subroutine ematq
+
+subroutine ematqk1(iq,ik)
+  use modmain
+  use modxs
+  use modmpi
+  use m_putemat
+  implicit none
+  ! arguments
+  integer, intent(in) :: iq,ik
+  ! set band combinations
+  call ematbdlims(2*emattype,nst1,istlo1,isthi1,nst2,istlo2,isthi2)
+  allocate(xiou(nst1,nst2,ngq(iq)))
+  call ematqk(iq,ik)
+  if (emattype.eq.0) then
+     ! all band combinations
+     nst3=nstsv; nst4=nstsv
+     call putemat(iq,ik,.false.,trim(fnemat_t),x1=xiou)
+  else
+     ! v-c/c-v or v-v/c-c band combinations
+     allocate(xiuo(nst1,nst2,ngq(iq)))
+     xiuo(:,:,:)=xiou(:,:,:)
+     deallocate(xiou)
+     call ematbdlims(2*emattype-1,nst1,istlo1,isthi1,nst2,istlo2,isthi2)
+     nst3=nst2; nst4=nst1
+     allocate(xiou(nst1,nst2,ngq(iq)))
+     call ematqk(iq,ik)
+     call putemat(iq,ik,.false.,trim(fnemat_t),x1=xiou,x2=xiuo)
+  end if
+  deallocate(xiou)
+  if (allocated(xiuo)) deallocate(xiuo)
+end subroutine ematqk1
+
+subroutine ematqalloc
+  use modmain
+  use modmpi
+  use modxs
+  implicit none
   ! allocate eigenvalue and eigenvector arrays
   if (allocated(evecfv)) deallocate(evecfv)
   allocate(evecfv(nmatmax,nstfv,nspnfv))
@@ -41,8 +93,6 @@ subroutine ematq(iq)
   allocate(evecfv0(nmatmax0,nstfv,nspnfv))
   if (allocated(evalsv0)) deallocate(evalsv0)
   allocate(evalsv0(nstsv,nkpt))
-  if (allocated(occsv0)) deallocate(occsv0)
-  allocate(occsv0(nstsv,nkpt))
   ! allocate helper matrix
   if (allocated(xih)) deallocate(xih)
   allocate(xih(nlotot,nlotot))
@@ -51,46 +101,6 @@ subroutine ematq(iq)
   allocate(apwdlm(nstsv,apwordmax,lmmaxapwtd,natmtot))
   if (allocated(apwdlm0)) deallocate(apwdlm0)
   allocate(apwdlm0(nstsv,apwordmax,lmmaxapwtd,natmtot))
-  ! delete timing information of previous runs
-  call filedel(trim(fnetim))
-  ! write information
-  write(unitout,'(a,i6)') 'Info('//thisnam//'): number of G+q vectors:', &
-       ngq(iq)
-  ! loop over k-points
-  do ik=kpari,kparf
-     ! set band combinations
-     call ematbdlims(2*emattype,nst1,istlo1,isthi1,nst2,istlo2,isthi2)
-     allocate(xiou(nst1,nst2,ngq(iq)))
-     call ematqk(iq,ik)
-     if (emattype.eq.0) then
-        ! all band combinations
-        nst3=nstsv; nst4=nstsv
-        call putemat(iq,ik,.false.,trim(fnemat_t),x1=xiou)
-     else
-        ! v-c/c-v or v-v/c-c band combinations
-        allocate(xiuo(nst1,nst2,ngq(iq)))
-        xiuo(:,:,:)=xiou(:,:,:)
-        deallocate(xiou)
-        call ematbdlims(2*emattype-1,nst1,istlo1,isthi1,nst2,istlo2,isthi2)
-        nst3=nst2; nst4=nst1
-        allocate(xiou(nst1,nst2,ngq(iq)))
-        call ematqk(iq,ik)
-        call putemat(iq,ik,.false.,trim(fnemat_t),x1=xiou,x2=xiuo)
-     end if
-     deallocate(xiou)
-     if (allocated(xiuo)) deallocate(xiuo)
-     ! synchronize for common number of k-points to all processes
-     if ((partype.eq.'k').and.(ik-kpari+1 <= nkpt/procs)) call barrier
-     ! end loop over k-points
-  end do
-end subroutine ematq
-
-subroutine ematqalloc
-  use modmain
-  use modmpi
-  use modxs
-  implicit none
-
 end subroutine ematqalloc
 
 subroutine ematqdealloc
@@ -98,5 +108,5 @@ subroutine ematqdealloc
   use modmpi
   use modxs
   implicit none
-
+  deallocate(evecfv,evecfv0,evalsv0,xih,apwdlm,apwdlm0)
 end subroutine ematqdealloc
