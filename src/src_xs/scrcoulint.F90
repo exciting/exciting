81818c9b0aabf9bdc503300a3098d4d97c20e437
Stephan Sagmeister <stephan.sagmeister@gmail.com>
2008-02-12 00:47:37 +0100

    mapping to first Brillouin zone

--- src/src_xs/scrcoulint.F90
+++ src/src_xs/scrcoulint.F90
@@ -253,14 +253,14 @@ subroutine mapto1bz(vl,vl1bz)
   real(8) :: v0(3),v1(3)
   ! map the q-vector into the first Brillouin zone
   t1=1.d8
-  v0(:)=0.d0
   do i1=-1,1
      do i2=-1,1
         do i3=-1,1
            v0=vl+dble((/i1,i2,i3/))
            v1=matmul(bvec,v0)
            t2=v1(1)**2+v1(2)**2+v1(3)**2
-           if (t2.lt.(t1)) then
+           ! favour positive values
+           if (t2.lt.(t1+1.d-8)) then
               t1=t2
               vl1bz(:)=v0(:)
            end if
