ed8af4274ddd3d4dfc5669047828aee4d663b7a0
Christian Meisenbichler <christian.meisenbichler@unileoben.ac.at>
2008-05-15 12:03:32 +0200

    combined normalisation with hs setup

--- src/src_iterative_solver/src_diis/setuphsvect.F90
+++ src/src_iterative_solver/src_diis/setuphsvect.F90
@@ -4,13 +4,22 @@ subroutine setuphsvect(n,m,system,evecfv,ldv,h,s)
   use modmain, only : nstfv,zone,zzero
   implicit none
   integer ,intent(in):: n,m,ldv
-  complex(8), intent(in):: evecfv(ldv,nstfv)
+  complex(8), intent(inout):: evecfv(ldv,nstfv)
   type(evsystem)::system
   complex(8), intent(out)::h(n,nstfv),s(n,nstfv)
-
-  call zhemm('L','U',n,m,zone,system%hamilton%za(1,1),n,evecfv(1,1),ldv,&
-     zzero,h(1,1),n)
+integer ::i
+complex(8)::z
+ 
   call zhemm('L','U',n,m,zone,system%overlap%za(1,1),n,evecfv(1,1),ldv,&
-      zzero,s(1,1),n)
-    
+       zzero,s(1,1),n)
+  Do i=1,m
+  	z =sqrt(dble (zdotc(n,evecfv(1,i),1,s(1,i),1)))
+    z=1.d0/z
+    write(*,*)"z",z
+  	call zscal(n,z,evecfv(1,i),1)
+  	call zscal(n,z,s(1,i),1)  
+  end do
+   call zhemm('L','U',n,m,zone,system%hamilton%za(1,1),n,evecfv(1,1),ldv,&
+     zzero,h(1,1),n)
+ 
 end subroutine setuphsvect
