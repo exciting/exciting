0b677d44e468c1061672fe2caf77970c3c2bba2b
Christian Meisenbichler <ChristianMeisenbichler@unileoben.ac.at>
2008-09-01 16:54:21 +0200

    arpack fails on gfortran and inte don't understand why

--- test/test02/src/test2.f90
+++ test/test02/src/test2.f90
@@ -1,14 +1,14 @@
 program test
 use modreport
 testplan_name ="test2"
-call inittestoutputfile()
+!call inittestoutputfile()
 
 ! list test routines here and call testreport(testunit,input,output,passed)
 ! before leaving the routine
-call system("rm *.OUT")
-call test_readinput()
-call test_gndstate_init()
+!call system("rm *.OUT")
+call readinput()
+call gndstate()
 call write_evec_formatted()
 
-call finalizeoutput()
+!call finalizeoutput()
 end program
