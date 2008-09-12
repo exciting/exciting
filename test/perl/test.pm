bbda7c5c0c8a15461a12ba6262c74c9fe803afe7
Christian Meisenbichler <christian.meisenbichler@unileoben.ac.at>
2008-09-12 14:48:49 +0200

    generate simple xml report with xml write

--- test/perl/test.pm
+++ test/perl/test.pm
@@ -1,8 +1,28 @@
-sub assert_file_same_within($file1,file2){
+sub assert_file_same_within
+{
+	$file1=$_[0],file2=$_[1]
+	open FILE1 $file1
+	open FILE2 $file2
+	while(<FILE1>)
+	{
+		$linefile1=$_
+		$linefile2=<fILE2>
+		while($linefile1=~m/$number/)
+		{
+			push(@numbers1,$_)	
+			$linefile2=~m/$number/
+			push(@numbers2,$_)
+		}
+	}
 
-%status=("status" => $status,
+if(max(@numbers1-@numbers2)>$tol)
+{
+ $status=0
+ $maxerror=max(@numbers1-@numbers2)
+ }
+%test=("status" => $status,
 	"line" => $linenr,
-	"column" => $collumn,
+	"column" => $collumn, 
 	"maxerror"=> $maxerror,
 	"averageerror"=> $averageerr
 	);
--- test/test02/assert.pl
+++ test/test02/assert.pl
@@ -1,18 +1,31 @@
 
-use XML::Simple
+use XML::Simple;
+use XML::Writer;
+use IO::File;
+
+  my $output = new IO::File(">output.xml");
+
+  my $writer = new XML::Writer(OUTPUT => $output,DATA_MODE => 'true', DATA_INDENT => 2);
+  $writer->xmlDecl( 'UTF-8' );
+  $writer->startTag("report");
+
+
+  $writer->startTag("test", 
+                    "directory" => "test01", name=>"my name to test");
+  $writer->startTag("description");     
+  $writer->characters("Hello, world! ><Üeß");
+  $writer->endTag("description");
+  $writer->startTag("status");
+  $writer->characters("passed");
+  $writer->endTag("status");  
+  $writer->endTag("test");  
+
+
+  $writer->endTag("report");
+  $writer->end();
+  $output->close();
 my @report;
+
 #do things to assert test
 
 
-push (@report,{
-	"name"->"",
-	"status"=>"",
-	"description"=>""
-	});	
-
-#repeat
-my $xsimple = XML::Simple->new();
-
-print $xsimple->XMLout(\%report,
-                       noattr => 1,
-                       xmldecl => '<?xml version="1.0">');
