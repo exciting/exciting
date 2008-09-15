63fbf3e1dcca4f0f887c175bff05764a5fa58197
Christian Meisenbichler <christian.meisenbichler@unileoben.ac.at>
2008-09-15 15:23:27 +0200

    wrote perl xml subroutines for testing and some simple tests

---
+++ test/perl/Test.pm
@@ -0,0 +1,75 @@
+package Test;
+use XML::Simple;
+use XML::Writer;
+use IO::File;
+use List::Util qw[min max];
+
+sub assert_file_same_within {
+	$file1=@_[0];
+	$file2=@_[1];
+	$tol=@_[2];
+	open FILE1,$file1;
+	open FILE2, $file2;
+	while(<FILE1>)
+	{
+		$linefile1=$_;
+		$linefile2=<FILE2>;
+			while ($linefile1=~s/([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)//){
+			$n1=$1;
+			push(@numbers1,$n1)	;
+			$linefile2=~s/([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)//;
+			$n2=$1;
+			push(@numbers2,$n2);
+			push(@error,abs($n1-$n2));
+			
+ 		}
+		
+	}
+	$status=failed;
+if(max( @error) <$tol)
+	{
+ 	$status=passed;
+ 	}
+ 	
+ 	$maxerror=max(@error );
+
+%test=("status" => $status,
+	"line" => $linenr,
+	"column" => $collumn, 
+	"maxerror"=> $maxerror,
+	"averageerror"=> $averageerr
+	);
+return %test;
+}
+
+sub initreport{ #call with filename
+  my $output = new IO::File(">@_[0]");
+  my $writer = new XML::Writer(OUTPUT => $output,DATA_MODE => 'true', DATA_INDENT => 2);
+  $writer->xmlDecl( 'UTF-8' );
+  $writer->startTag("report");
+return $writer
+ }
+sub closereport{
+$writer=@_[0] ;
+  $writer->endTag("report");
+  $writer->end();
+  $output=$writer-> getOutput();
+  $output->close();
+  }
+  
+sub writetestreport(%$) { #hash with values and writer object
+$writer=@_[1];
+  $writer->startTag("test", 
+                    "directory" => @_[0]->{directory}, 
+                    "name"=>@_[0]->{name});
+  $writer->startTag("description");     
+  $writer->characters(@_[0]->{description});
+  $writer->endTag("description");
+  $writer->startTag("status");
+  $writer->characters(@_[0]->{status});
+  $writer->endTag("status");  
+  $writer->endTag("test");  
+return 1;
+}
+
+return 1
--- test/test02/assert.pl
+++ test/test02/assert.pl
@@ -1,31 +1,83 @@
-
+use lib "../perl/";
 use XML::Simple;
 use XML::Writer;
 use IO::File;
+use Test;
 
-  my $output = new IO::File(">output.xml");
-
-  my $writer = new XML::Writer(OUTPUT => $output,DATA_MODE => 'true', DATA_INDENT => 2);
-  $writer->xmlDecl( 'UTF-8' );
-  $writer->startTag("report");
-
-
-  $writer->startTag("test", 
-                    "directory" => "test01", name=>"my name to test");
-  $writer->startTag("description");     
-  $writer->characters("Hello, world! ><Üeß");
-  $writer->endTag("description");
-  $writer->startTag("status");
-  $writer->characters("passed");
-  $writer->endTag("status");  
-  $writer->endTag("test");  
-
-
-  $writer->endTag("report");
-  $writer->end();
-  $output->close();
-my @report;
-
+$writer= Test::initreport("report.xml");
 #do things to assert test
 
+open INFO, "runarp/INFO.OUT";
+$status=failed;
+while(<INFO>)
+	{
+	if (m/\| EXCITING version.+stopped/){
+		$status="passed";
+	}
+}
+close INFO;
 
+
+ Test::writetestreport({
+ 		directory=>"test02/runarp",
+ 		name=>"arpackrun",
+ 		description=>"The test run  using arpack finished without errors",
+ 		status=>$status
+ 		}, $writer);
+ 
+ open INFO, "runlapack/INFO.OUT";
+$status=failed;
+while(<INFO>)
+	{
+	if (m/\| EXCITING version.+stopped/){
+		$status="passed";
+	}
+}
+close INFO;
+
+
+ Test::writetestreport({
+ 		directory=>"test02/runlapack",
+ 		name=>"laackrun",
+ 		description=>"The test run  using lapack finished without errors",
+ 		status=>$status
+ 		}, $writer);
+ 
+
+
+
+#compare total energies
+$tol=1e-9;
+open TOTEARP, "runarp/TOTENERGY.OUT";
+open TOTELAP, "runlapack/TOTENERGY.OUT";
+$status="failed";
+$totearp= <TOTEARP>;
+$totelap= <TOTELAP>;
+$err=$totearp-$totelap;
+if ($totearp-$totelap<=$tol){
+$status="passed";
+}
+ Test::writetestreport({
+ 		directory=>"test02/ ",
+ 		name=>"totalenergy compare LAPACK ARPACK ",
+ 		description=>"The test is passed if the total energy differs\
+ 		 less than $tol between lapack and Arpack 
+ 		 difference=$err",
+ 		status=>$status
+ 		}, $writer);
+
+#compare eigenvalues
+$tol=1e-8;
+%statuseigval=Test::assert_file_same_within( "runarp/EIGVAL.OUT",
+	"runlapack/EIGVAL.OUT",$tol);
+ Test::writetestreport({
+ 		"directory"=>"test02/ ",
+ 		"name"=>"eigenvalue comparison LAPACK ARPACK ",
+ 		"description"=>"The test is passed if the eigenvalues  differ
+ 		 less than $tol between lapack and Arpack 
+ 		 difference=" .  %statuseigval->{maxerror},
+ 		"status"=> %statuseigval->{status}
+ 		}, $writer);
+
+
+ Test::closereport($writer);
