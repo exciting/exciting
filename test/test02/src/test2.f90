bc80fe37273de5b6deda4af75197b8862382f905
Christian Meisenbichler <ChristianMeisenbichler@unileoben.ac.at>
2008-10-09 09:38:15 +0200

    add mpi test seed

--- test/test02/src/test2.f90
+++ test/test02/src/test2.f90
@@ -1,5 +1,6 @@
 program test
 use modreport
+use modmpi
 testplan_name ="test2"
 !call inittestoutputfile()
 
@@ -8,7 +9,8 @@ testplan_name ="test2"
 !call system("rm *.OUT")
 call readinput()
 call gndstate()
+if (rank.eq.0) then
 call write_evec_formatted()
-
+endif
 !call finalizeoutput()
 end program
