5c1d4586647d3996185a33843539678558558f12
Christian Meisenbichler <ChristianMeisenbichler@unileoben.ac.at>
2008-10-10 13:41:43 +0200

    added test for new mixer

--- test/test03/report.pl
+++ test/test03/report.pl
@@ -39,4 +39,33 @@ close INFO;
  		status=>$status
  		}, $writer);
  		
+ 		
+ 		open INFO, "runmixer2/INFO.OUT";
+$status=failed;
+while(<INFO>)
+	{
+	if (m/\| EXCITING version.+stopped/){
+		$status="passed";
+	}
+	if (m/Iteration number :   (\d+)/){
+	$iterations=$1;
+}
+}
+Test::writetestreport({
+ 		directory=>"test03/runmixer2",
+ 		name=>"debug binary run mixer2",
+ 		description=>"The test run  using arpack finished without errors",
+ 		status=>$status
+ 		}, $writer);
+ 		
+ 		$iterationsref=13;
+ 		if ($iterations=13){$passed="true";}else{$passed="false";}
+ 		
+ 		Test::writetestreport({
+ 		directory=>"test03/runmixer2",
+ 		name=>"check iteration number of Al for mixer2",
+ 		description=>"iterations is $iterations reference is $iterationsref",
+ 		status=>"unspecified"
+ 		}, $writer);
+ 		
  Test::closereport($writer);
