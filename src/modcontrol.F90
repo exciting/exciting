a71563562bcc8b3ebe2e02d02b42a957d4c66ca3
Stephan Sagmeister <stephan.sagmeister@gmail.com>
2009-03-12 16:48:47 +0100

    removed modcontrol.F90 - deprecated

--- src/modcontrol.F90
+++
@@ -1,9 +0,0 @@
-module controls
-  implicit none
-  integer, save :: init0_c
-  data init0_c /0/
-  integer, save :: genshtmat_c
-  data genshtmat_c /0/
-  integer, save :: sphcover_c
-  data sphcover_c /0/
-end module controls
