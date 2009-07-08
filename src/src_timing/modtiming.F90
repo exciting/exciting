6093428b3206f1121acc2aea1ba40ee67755512f
Christian Meisenbichler <christian.meisenbichler@unileoben.ac.at>
2009-07-08 15:13:18 +0200

    tining

--- src/src_timing/modtiming.F90
+++ src/src_timing/modtiming.F90
@@ -1,4 +1,5 @@
 
+
 ! Copyright (C) 2008 S. Sagmeister and C. Ambrosch-Draxl.
 ! This file is distributed under the terms of the GNU General Public License.
 ! See the file COPYING for license details.
@@ -34,7 +35,8 @@ module modtimer
   private :: new_interval
 contains
 
-  subroutine new_timer(t,typ,name)
+
+subroutine new_timer(t, typ, name)
     implicit none
     type(timer) :: t
     character(*), intent(in) :: typ
@@ -49,63 +51,68 @@ contains
     t%int_large=-1.d10
     t%int_mean=0.d0
     select case (trim(adjustl(typ)))
-    case('system','wall')
+    case('system', 'wall')
        t%systemclock=.true.
     case('cpu')
        t%systemclock=.false.
     case default
-       write(*,*)
-       write(*,'("Error(timing:new_timer): unknown clocktype: ",a)') &
-            trim(typ)
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:new_timer): unknown clocktype: ", a)') &
+	    trim(typ)
+       write(*, *)
        stop
     end select
   end subroutine new_timer
 
-  subroutine tic_timer(t)
+
+subroutine tic_timer(t)
     implicit none
     type(timer) :: t
     if (t%running) then
-       call new_interval(t,get_time(t))
+       call new_interval(t, get_time(t))
     else
        t%running=.true.
        t%mark=get_time(t)
     end if
   end subroutine tic_timer
 
-  subroutine toc_timer(t)
+
+subroutine toc_timer(t)
     implicit none
     type(timer) :: t
     if (.not.t%running) then
-       write(*,'("Error(timing:toc_timer): timer already stopped")')
+       write(*, '("Error(timing:toc_timer): timer already stopped")')
        stop
     end if
-    call new_interval(t,get_time(t))
+    call new_interval(t, get_time(t))
     t%running=.false.
   end subroutine toc_timer
 
-  subroutine report_timer(t,un)
+
+subroutine report_timer(t, un)
+use modinput
     implicit none
+
     type(timer) :: t
     integer, intent(in) :: un
-    write(un,'("Timer: ",a)') trim(t%name)
+    write(un, '("Timer: ", a)') trim(t%name)
     if (t%running) then
-       write(un,'(" timer is running")')
+       write(un, '(" timer is running")')
     else
-       write(un,'(" timer is stopped")')
+       write(un, '(" timer is stopped")')
     end if
     if (t%systemclock) then
-       write(un,'(" timer is using system-clock")')
+       write(un, '(" timer is using system-clock")')
     else
-       write(un,'(" timer is using CPU-clock")')
+       write(un, '(" timer is using CPU-clock")')
     end if
-    write(un,'(" elapsed time      : ",g18.10)') t%elapsed
-    write(un,'(" intervals         : ",i8)') t%intervals
-    write(un,'(" last interval     : ",g18.10)') t%int_last
-    write(un,'(" mean interval     : ",g18.10)') t%int_mean
-    write(un,'(" smallest interval : ",g18.10)') t%int_small
-    write(un,'(" largest  interval : ",g18.10)') t%int_large
-    write(un,'(" current mark      : ",g18.10)') t%mark
+    write(un, '(" elapsed time	    : ", g18.10)') t%elapsed
+    write(un, '(" intervals	    : ", i8)') t%intervals
+    write(un, '(" last interval     : ", g18.10)') t%int_last
+    write(un, '(" mean interval     : ", g18.10)') t%int_mean
+    write(un, '(" smallest interval : ", g18.10)') t%int_small
+    write(un, '(" largest  interval : ", g18.10)') t%int_large
+    write(un, '(" current mark	    : ", g18.10)') t%mark
   end subroutine report_timer
 
   character(256) function get_name_timer(t)
@@ -177,7 +184,9 @@ contains
   !
   !private routines
   !
-  subroutine new_interval(t,s)
+
+
+subroutine new_interval(t, s)
     implicit none
     type(timer) :: t
     real(8), intent(in) :: s
@@ -187,8 +196,8 @@ contains
     t%mark=s
     t%intervals=t%intervals+1
     t%int_last=sint
-    t%int_small=min(t%int_small,sint)
-    t%int_large=max(t%int_large,sint)
+    t%int_small=min(t%int_small, sint)
+    t%int_large=max(t%int_large, sint)
     t%int_mean=(dble(t%intervals-1)*t%int_mean+sint)/dble(t%intervals)
   end subroutine new_interval
 
@@ -204,8 +213,8 @@ contains
 
   real(8) function get_system_time()
     implicit none
-    integer :: cnt,cntr
-    call system_clock(count=cnt,count_rate=cntr)
+    integer :: cnt, cntr
+    call system_clock(count=cnt, count_rate=cntr)
     get_system_time=dble(cnt)/dble(cntr)
   end function get_system_time
 
@@ -234,7 +243,8 @@ module modtimer2
 
 contains
 
-  subroutine new_timer2(t,typ,name)
+
+subroutine new_timer2(t, typ, name)
     implicit none
     type(timer2) :: t
     character(*), intent(in), optional :: typ
@@ -245,54 +255,57 @@ contains
     if (present(typ)) then
     select case (trim(adjustl(typ)))
        case('combined')
-       case('system','wall')
-          t%tcpu=.false.
+       case('system', 'wall')
+	  t%tcpu=.false.
        case('cpu')
-          t%tsys=.false.
+	  t%tsys=.false.
        case default
-          write(*,*)
-          write(*,'("Error(timing:new_timer2): unknown type: ",a)') trim(typ)
-          write(*,*)
-          stop
+	  write(*, *)
+	  write(*, '("Error(timing:new_timer2): unknown type: ", a)') trim(typ)
+	  write(*, *)
+	  stop
     end select
     end if
     t%name='unnamed'
     if (present(name)) t%name=trim(name)
-    if (t%tsys) call new_timer(t%sys,'system','combined:System')
-    if (t%tcpu) call new_timer(t%cpu,'cpu','combined:CPU')
+    if (t%tsys) call new_timer(t%sys, 'system', 'combined:System')
+    if (t%tcpu) call new_timer(t%cpu, 'cpu', 'combined:CPU')
   end subroutine new_timer2
-  
-  subroutine tic_timer2(t)
+
+
+subroutine tic_timer2(t)
     implicit none
     type(timer2) :: t
     if (t%tsys) call tic_timer(t%sys)
     if (t%tcpu) call tic_timer(t%cpu)
   end subroutine tic_timer2
 
-  subroutine toc_timer2(t)
+
+subroutine toc_timer2(t)
     implicit none
     type(timer2) :: t
     if (t%tsys) call toc_timer(t%sys)
     if (t%tcpu) call toc_timer(t%cpu)
   end subroutine toc_timer2
 
-  subroutine report_timer2(t,un,string)
+
+subroutine report_timer2(t, un, string)
     implicit none
     type(timer2) :: t
     integer, intent(in) :: un
     character(*), intent(in), optional :: string
     real(8) :: r
-    write(un,*)
-    write(un,'("Combined timer; summary below")')
-    if (present(string)) write(un,'(" ID                : ",a)') trim(string)
-    if (t%tsys) call report_timer(t%sys,un)
-    if (t%tcpu) call report_timer(t%cpu,un)
+    write(un, *)
+    write(un, '("Combined timer; summary below")')
+    if (present(string)) write(un, '(" ID		 : ", a)') trim(string)
+    if (t%tsys) call report_timer(t%sys, un)
+    if (t%tcpu) call report_timer(t%cpu, un)
     if (t%tsys.and.t%tcpu) then
       r=get_elapsed_timer(t%sys)
       if (r.ne.0.d0) r=get_elapsed_timer(t%cpu)/r
-      write(un,'(" CPU/System (%)    : ",g18.10)') r*100.d0
+      write(un, '(" CPU/System (%)    : ", g18.10)') r*100.d0
     end if
-    write(un,*)
+    write(un, *)
   end subroutine report_timer2
 
   character(256) function get_name_timer2(t)
@@ -319,17 +332,17 @@ contains
     if (t%tsys) is_running_timer2=is_running_timer(t%sys)
     if (t%tcpu) is_running_timer2=is_running_timer(t%cpu)
   end function is_running_timer2
-  
+
   real(8) function get_elapsed_system_timer2(t)
     implicit none
     type(timer2) :: t
     if (t%tsys) then
        get_elapsed_system_timer2=get_elapsed_timer(t%sys)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_elapsed_system_timer2): no System clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_elapsed_system_timer2): no System clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_elapsed_system_timer2
@@ -340,10 +353,10 @@ contains
     if (t%tcpu) then
        get_elapsed_cpu_timer2=get_elapsed_timer(t%cpu)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_elapsed_cpu_timer2): no CPU clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_elapsed_cpu_timer2): no CPU clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_elapsed_cpu_timer2
@@ -364,10 +377,10 @@ contains
     if (t%tsys) then
        get_mark_system_timer2=get_mark_timer(t%sys)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_mark_system_timer2): no System clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_mark_system_timer2): no System clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_mark_system_timer2
@@ -378,10 +391,10 @@ contains
     if (t%tcpu) then
        get_mark_cpu_timer2=get_mark_timer(t%cpu)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_mark_cpu_timer2): no CPU clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_mark_cpu_timer2): no CPU clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_mark_cpu_timer2
@@ -402,17 +415,17 @@ contains
     if (t%tsys) get_intervals_timer2=get_intervals_timer(t%sys)
     if (t%tcpu) get_intervals_timer2=get_intervals_timer(t%cpu)
   end function get_intervals_timer2
-  
+
   real(8) function get_intlast_system_timer2(t)
     implicit none
     type(timer2) :: t
     if (t%tsys) then
        get_intlast_system_timer2=get_intlast_timer(t%sys)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_intlast_system_timer2): no System clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_intlast_system_timer2): no System clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_intlast_system_timer2
@@ -423,10 +436,10 @@ contains
     if (t%tcpu) then
        get_intlast_cpu_timer2=get_intlast_timer(t%cpu)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_intlast_cpu_timer2): no CPU clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_intlast_cpu_timer2): no CPU clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_intlast_cpu_timer2
@@ -447,10 +460,10 @@ contains
     if (t%tsys) then
        get_intsmall_system_timer2=get_intsmall_timer(t%sys)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_intsmall_system_timer2): no System clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_intsmall_system_timer2): no System clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_intsmall_system_timer2
@@ -461,10 +474,10 @@ contains
     if (t%tcpu) then
        get_intsmall_cpu_timer2=get_intsmall_timer(t%cpu)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_intsmall_cpu_timer2): no CPU clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_intsmall_cpu_timer2): no CPU clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_intsmall_cpu_timer2
@@ -485,10 +498,10 @@ contains
     if (t%tsys) then
        get_intlarge_system_timer2=get_intlarge_timer(t%sys)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_intlarge_system_timer2): no System clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_intlarge_system_timer2): no System clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_intlarge_system_timer2
@@ -499,10 +512,10 @@ contains
     if (t%tcpu) then
        get_intlarge_cpu_timer2=get_intlarge_timer(t%cpu)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_intlarge_cpu_timer2): no CPU clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_intlarge_cpu_timer2): no CPU clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_intlarge_cpu_timer2
@@ -523,10 +536,10 @@ contains
     if (t%tsys) then
        get_intmean_system_timer2=get_intmean_timer(t%sys)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_intmean_system_timer2): no System clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_intmean_system_timer2): no System clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_intmean_system_timer2
@@ -537,10 +550,10 @@ contains
     if (t%tcpu) then
        get_intmean_cpu_timer2=get_intmean_timer(t%cpu)
     else
-       write(*,*)
-       write(*,'("Error(timing:get_intmean_cpu_timer2): no CPU clock &
-       	&timer running")')
-       write(*,*)
+       write(*, *)
+       write(*, '("Error(timing:get_intmean_cpu_timer2): no CPU clock &
+	&timer running")')
+       write(*, *)
        stop
     end if
   end function get_intmean_cpu_timer2
@@ -554,5 +567,5 @@ contains
        get_intmean_timer2=get_intmean_cpu_timer2(t)
     end if
   end function get_intmean_timer2
-  
+
 end module modtimer2
