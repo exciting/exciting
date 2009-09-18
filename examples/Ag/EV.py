23734db84ad1469bd312d2e915f7b9d3ee9bfc15
Christian Meisenbichler <christian.meisenbichler@unileoben.ac.at>
2009-09-18 14:29:10 +0200

    deleted some obsolete files

--- examples/Ag/EV.py
+++ examples/Ag/EV.py
@@ -9,10 +9,12 @@ ctxt = eVdoc.xpathNewContext()
 Volume=nm.array(map(float,map(xmlAttr.getContent,ctxt.xpathEval("//@volume"))))
 totalEnergy=nm.array(map(float,map(xmlAttr.getContent,ctxt.xpathEval("//@totalEnergy"))))
 # make quadratic fit
-p=nm.polyfit(Volume,totalEnergy,2)
+p=nm.polyfit(Volume,totalEnergy,3)
 curve=nm.poly1d(p)
 # find root of derivative to get minimum
-min=nm.roots(nm.polyder(p))
+minv=nm.roots(nm.polyder(p))[1]
+print 'minimum Volume '+str(minv)
+print 'minimum energy at scale '+str(pow(minv/2,1./3.))
 # x values for plotting polynomial
 xa = nm.linspace(Volume[0],Volume[-1],100)
 #plot
@@ -22,12 +24,11 @@ plt.ylabel(r'total energy in $[Hartree]$')
 plt.xlabel(r'volume in $[Bohr]^3$')
 plt.plot(xa,curve(xa),'-')
 plt.plot(Volume,totalEnergy,'o')
-plt.plot(min,curve(min),'o')
-plt.annotate('minimum Volume '+str(min[0]), xy=(min,curve(min)), xycoords='data' ,
-              xytext=(min-10,curve(min)+0.002) ,  arrowprops=dict(arrowstyle="->"))
-print 'minimum Volume '+str(min[0])
+plt.annotate('minimum Volume '+str(minv), xy=(minv,curve(minv)), xycoords='data' ,
+              xytext=(minv-7,curve(minv)+0.002) ,  arrowprops=dict(arrowstyle="->"))
+
 
 plt.savefig('EV.png')
-plt.savefig('EV.pdf')
+
 print 'plot saved as EV.png'
 plt.show()
