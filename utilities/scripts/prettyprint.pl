8fbc6ace56a57b7aec48458c1cb3ad6361ae3ec6
Christian Meisenbichler <christian.meisenbichler@unileoben.ac.at>
2009-11-25 10:31:03 +0100

    prettyprint/reindent script more save

--- utilities/scripts/prettyprint.pl
+++ utilities/scripts/prettyprint.pl
@@ -14,14 +14,20 @@ print @fileslist;
 
 foreach $file (@fileslist) {
 	$return=-1;
-	$command=  "f90ppr < $file > ./tmp.f90";
+	$command=  "f90ppr < $file > ./pptmp";
 	print $command ,"\n";
 	$return= system  $command;
 	print "return value ",$return,"\n";
 	if ($return==0){
-		$mvcommand="mv ./tmp.f90 $file";
+		$diffcommand="diff $file  ./pptmp ";
+		$diff=`$diffcommand`;
+		print "diff is:", $diff,"\n";
+		if(not ($diff eq "")){
+		$mvcommand="mv ./pptmp $file";
 		print $mvcommand ,"\n";
 		system $mvcommand;
-		
+		}else{
+		system "rm pptmp"	
+		}
 	}
 }
