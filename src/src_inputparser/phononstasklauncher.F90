6bd24ad7cba29b5579f16e2ca1d73aff30301532
Stephan Sagmeister <stephan.sagmeister@gmail.com>
2010-10-19 16:47:08 +0200

    <reformatdynmat /> for nicer formatting of dynamical matrices within the <phonon> element

--- src/src_inputparser/phononstasklauncher.F90
+++ src/src_inputparser/phononstasklauncher.F90
@@ -38,7 +38,9 @@ Subroutine phononstasklauncher
         ! task 201 is only a dry-run and will not be considered here
         If (input%phonons%do .Ne. "skip") then
             Call phonon
-!(sag)            call reformatdynamicalmatrices
+        end if
+        if (associated(input%phonons%reformatdynmat)) then
+            call reformatdynamicalmatrices
         end if
         if (associated(input%phonons%qpointset)) then
             task=230
