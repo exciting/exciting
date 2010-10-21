1fb0867d2e63a4c492368384109ed8a119934f43
Stephan Sagmeister <stephan.sagmeister@gmail.com>
2010-10-21 10:59:27 +0200

    symmetry analysis of independent components of a rank 2 tensor

--- src/src_xs/writesymt2.F90
+++ src/src_xs/writesymt2.F90
@@ -33,10 +33,50 @@ Subroutine writesymt2
   ! local variables
       real(8), parameter :: eps=1.d-8
       character(256) :: strvar
-      Integer :: iop1, iop2, i
-      real(8) :: t1
+      character(4) :: dm(3,3), dmt(3,3)
+      Integer :: iop1, iop2, jop1, jop2, i
+      real(8) :: t1, t2
+      logical :: red(3,3), done(3,3)
+      red(:,:)=.false.
+      done(:,:)=.false.
+      dm(1,:)=(/'e_11','e_12','e_13'/)
+      dm(2,:)=(/'e_21','e_22','e_23'/)
+      dm(3,:)=(/'e_31','e_32','e_33'/)
+      dmt(:,:)=dm(:,:)
+  ! analyse the symmetrization matrices
+      do iop1=1,3
+        do iop2=1,3
+          t2=sum(abs(symt2(iop1,iop2,:,:)))
+          do jop1=1,3
+            do jop2=1,3
+              if (.not.done(iop1,iop2)) then
+                t1=sum(abs(symt2(iop1,iop2,:,:)-symt2(jop1,jop2,:,:)))
+                if ((t1.lt.eps).and.(t2.gt.eps).and.((iop1.ne.jop1).or.(iop2.ne.jop2))) then
+                  dmt(jop1,jop2)=dm(iop1,iop2)
+                  red(jop1,jop2)=.true.
+                end if
+                done(jop1,jop2)=.true.
+             end if
+            end do
+          end do
+          if (sum(abs(symt2(iop1,iop2,:,:))).lt.eps) then
+            dmt(iop1,iop2)=' 0  '
+            red(iop1,iop2)=.true.
+          end if
+        end do
+      end do
   ! output the symmetrization matrices
       Open (50, File='SYMT2'//trim(filext), Action='WRITE', Form='FORMATTED')
+      Write (50,*)
+      Write (50, '("(structure of a rank 2 tensor, e.g. the dielectric tensor, &
+        &from symmetry considerations")')
+      Write (50, '(" with respect to Cartesian coordinates)")')
+      Write (50,*)
+      Write (50, '(" Maximum number of independent components : ",i6)') 9-count(red)
+      Write (50,*)
+      Do iop1 = 1, 3
+        write(50,'(" ( ",a,"  ",a,"  ",a," )")') dmt(iop1,:)
+      End Do
       Write (50,*)
       Write (50, '("(symmetrization matrices are in Cartesian coordinates)")')
       Write (50,*)
