996ab16f6f5d6fdd7d5ae93dbf10b9958c7f3f44
toxa81 <toxa81@gmail.com>
2012-04-24 16:23:56 +0200

    ultiple changes in libapw

---
+++ src/libapw/cpp/lapw_get_evec.cpp
@@ -0,0 +1,19 @@
+#include "lapw.h"
+
+extern "C" void FORTRAN(lapw_get_evec)(int32_t *ikloc_, complex16 *evecfv_, complex16 *evecsv_)
+{
+    int ikloc = *ikloc_ - 1;
+    
+    mdarray<complex16,2> evecfv(evecfv_, lapw_global.nmatmax, lapw_global.nstfv); 
+    mdarray<complex16,2> evecsv(evecsv_, lapw_global.nstsv, lapw_global.nstsv);
+    
+    for (unsigned int i = 0; i < lapw_global.nstfv; i++)
+    {
+        memcpy(&evecfv(0, i), &lapw_runtime.bloch_states[ikloc]->evecfv(0, i), lapw_runtime.bloch_states[ikloc]->lapw_basis_size * sizeof(complex16));
+    }
+
+    for (unsigned int i = 0; i < lapw_global.nstsv; i++)
+    {
+        memcpy(&evecsv(0, i), &lapw_runtime.bloch_states[ikloc]->evecsv(0, i), lapw_global.nstsv * sizeof(complex16));
+    }
+}
