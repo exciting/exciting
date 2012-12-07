e38827b6cbd069d303a5b2c0dc6fe7312cd891db
Dmitrii Nabok <dmitrii,nabok@gmail.com>
2012-12-07 15:38:02 +0100

    Species program has been modified to generate the new 'style' species. Corresponding changes has been made in readspeciesxml.f90 routine.

--- src/species/writexmlspecies.F90
+++ src/species/writexmlspecies.F90
@@ -5,12 +5,15 @@
 
 
 subroutine  writexmlspecies
+
   use FoX_wxml
   use modspecies
+
   implicit none
   type(xmlf_t), save::xf
   character(100)::buffer,buffer2
-  call xml_OpenFile (trim(spsymb)//'_'//trim(suffix)//'.xml', xf, replace=.true.,pretty_print=.true.)
+
+  call xml_OpenFile (trim(spsymb)//trim(suffix)//'.xml', xf, replace=.true.,pretty_print=.true.)
   call xml_NewElement (xf, "spdb")
   call xml_DeclareNamespace(xf, "http://www.w3.org/2001/XMLSchema-instance", "xsi")
   call xml_AddAttribute(xf, "xsi:noNamespaceSchemaLocation", "../../xml/species.xsd" )
@@ -31,7 +34,6 @@ subroutine  writexmlspecies
   write(buffer,*)nrmt
   call xml_AddAttribute(xf, "radialmeshPoints", trim(adjustl(buffer)))
   call xml_endElement(xf, "muffinTin")
-
 
   do ist=1,spnst
      call xml_NewElement (xf, "atomicState")
@@ -55,62 +57,132 @@ subroutine  writexmlspecies
 !--------------------------------------------------------------
 ! BASIS
 !--------------------------------------------------------------
-
   call xml_NewElement (xf, "basis")
+
+  if (trim(apwdescr).eq.'simple') then
 !
-! Default augmentation
+!   Default augmentation
 ! 
-! basis type
-  call xml_NewElement (xf, "default")
-  if (apword.eq.1) then
-    write(buffer,*) 'apw+lo'
-  else if (apword.eq.2) then
-    write(buffer,*) 'lapw'
-  else
-    write(*,*) 'Not supported basis type: apword>2'
-    stop
-  end if
-  call xml_AddAttribute(xf, "type", trim(adjustl(buffer)))
-
-! trial energy
-  write(buffer,'(F8.4)')boe
-  call xml_AddAttribute(xf, "trialEnergy", trim(adjustl(buffer)))
-
-! search LE
-  if(apwve(io)) then
-    call xml_AddAttribute(xf, "searchE", "true")
-  else
-    call xml_AddAttribute(xf, "searchE", "false")
-  endif
-  call xml_endElement (xf, "default")
-
+!   basis type
+    call xml_NewElement (xf, "default")
+    if (apword.eq.1) then
+      write(buffer,*) 'apw+lo'
+    else if (apword.eq.2) then
+      write(buffer,*) 'lapw'
+    else
+      write(*,*) 'Not supported basis type: apword>2'
+      stop
+    end if
+    call xml_AddAttribute(xf, "type", trim(adjustl(buffer)))
 !
-! Custom augmentation
+!   trial energy
+    write(buffer,'(F8.4)') boe
+    call xml_AddAttribute(xf, "trialEnergy", trim(adjustl(buffer)))
 !
-  if (apwordx.gt.0) then
-    do l = 0, maxl
-      call xml_NewElement (xf, "custom")
-      write(buffer,*) l
-      call xml_AddAttribute(xf, "l", trim(adjustl(buffer)))
-      if (apwordx.eq.1) then
-        write(buffer,*) 'apw+lo'
-        call xml_AddAttribute(xf, "type", trim(adjustl(buffer)))
-      else if (apwordx.eq.2) then
-        write(buffer,*) 'lapw'
-        call xml_AddAttribute(xf, "type", trim(adjustl(buffer)))
-      else
-        write(*,*) 'Not supported basis type: apwordx>2'
-        stop
-      end if
+!   search LE
+    if(apwve(1)) then
+      call xml_AddAttribute(xf, "searchE", "true")
+    else
+      call xml_AddAttribute(xf, "searchE", "false")
+    endif
+    call xml_endElement (xf, "default")
+!
+!   Custom augmentation type
+!
+    if (apwordx.gt.0) then
+      do l = 0, maxl
+        call xml_NewElement (xf, "custom")
+        write(buffer,*) l
+        call xml_AddAttribute(xf, "l", trim(adjustl(buffer)))
+        if (apwordx.eq.1) then
+          write(buffer,*) 'apw+lo'
+          call xml_AddAttribute(xf, "type", trim(adjustl(buffer)))
+        else if (apwordx.eq.2) then
+          write(buffer,*) 'lapw'
+          call xml_AddAttribute(xf, "type", trim(adjustl(buffer)))
+        else
+          write(*,*) 'Not supported basis type: apwordx>2'
+          stop
+        end if
+        write(buffer,'(F8.4)') boe
+        call xml_AddAttribute(xf, "trialEnergy", trim(adjustl(buffer)))
+        if (apwvex(1)) then
+          call xml_AddAttribute(xf, "searchE", "true")
+        else
+          call xml_AddAttribute(xf, "searchE", "false")
+        endif
+        call xml_endElement (xf, "custom")
+      end do ! l
+    end if
+  
+  else if (trim(apwdescr).eq.'advanced') then
+!
+!   Default augmentation
+! 
+!   basis type
+    call xml_NewElement (xf, "default")
+    do io=1,apword
+      call xml_NewElement (xf, "wf")
+      write(buffer,*) apwdm(io)
+      call xml_AddAttribute(xf, "matchingOrder", trim(adjustl(buffer)))
       write(buffer,'(F8.4)') boe
       call xml_AddAttribute(xf, "trialEnergy", trim(adjustl(buffer)))
-      if (apwvex(io)) then
+      if(apwve(io)) then
         call xml_AddAttribute(xf, "searchE", "true")
       else
         call xml_AddAttribute(xf, "searchE", "false")
       endif
-      call xml_endElement (xf, "custom")
-    end do ! l
+      call xml_endElement (xf, "wf")
+    end do
+    call xml_endElement (xf, "default")
+!
+!   Custom augmentation type
+!
+    if (apwordx.gt.0) then
+      do l = 0, maxl
+        call xml_NewElement (xf, "custom")
+        write(buffer,*) l
+        call xml_AddAttribute(xf, "l", trim(adjustl(buffer)))
+        do io = 1, apwordx
+          call xml_NewElement (xf, "wf")
+          write(buffer,*) apwdmx(io)
+          call xml_AddAttribute(xf, "matchingOrder", trim(adjustl(buffer)))
+          write(buffer,'(F8.4)') boe
+          call xml_AddAttribute(xf, "trialEnergy", trim(adjustl(buffer)))
+          if(apwvex(io)) then
+            call xml_AddAttribute(xf, "searchE", "true")
+          else
+            call xml_AddAttribute(xf, "searchE", "false")
+          endif
+          call xml_endElement(xf, "wf")
+        end do
+        call xml_endElement (xf, "custom")
+!
+!       print lo for APW+lo method
+!
+        if (locorb) then
+          buffer2="false"
+          if(searchlocorb) buffer2="true"
+            call xml_NewElement (xf, "lo")
+            write(buffer,*) l
+            call xml_AddAttribute(xf, "l", trim(adjustl(buffer)))
+            call xml_NewElement (xf, "wf")
+            call xml_AddAttribute(xf, "matchingOrder", "0")
+            write(buffer,'(F8.4)')boe
+            call xml_AddAttribute(xf, "trialEnergy", trim(adjustl(buffer)))
+            call xml_AddAttribute(xf, "searchE", trim(adjustl(buffer2)))
+            call xml_endElement (xf, "wf")
+            call xml_NewElement (xf, "wf")
+            call xml_AddAttribute(xf, "matchingOrder", "1")
+            write(buffer,'(F8.4)')boe
+            call xml_AddAttribute(xf, "trialEnergy", trim(adjustl(buffer)))
+            call xml_AddAttribute(xf, "searchE", trim(adjustl(buffer2)))
+            call xml_endElement (xf, "wf")
+            call xml_endElement (xf, "lo")
+         endif
+!
+      end do ! l
+    end if
   end if
 !
 ! Local Orbitals
@@ -123,7 +195,7 @@ subroutine  writexmlspecies
        if (.not.spcore(ist)) then
          if ((spl(ist).eq.0).or.(spl(ist).eq.spk(ist))) then
            if (eval(ist).lt.esccut) then
-             call xml_NewElement (xf, "lorb")
+             call xml_NewElement (xf, "lo")
              write(buffer,*)spl(ist)
              call xml_AddAttribute(xf, "l", trim(adjustl(buffer)))
              call xml_NewElement (xf, "wf")
@@ -140,11 +212,11 @@ subroutine  writexmlspecies
              call xml_endElement (xf, "wf")
              call xml_NewElement (xf, "wf")
              call xml_AddAttribute(xf, "matchingOrder", "0")
-             write(buffer,'(F8.4)') eval(ist)+0.5d0*boe
+             write(buffer,'(F8.4)') eval(ist)
              call xml_AddAttribute(xf, "trialEnergy", trim(adjustl(buffer)))
              call xml_AddAttribute(xf, "searchE", "true")
              call xml_endElement (xf, "wf")
-             call xml_endElement (xf, "lorb")
+             call xml_endElement (xf, "lo")
            end if
          end if
        end if
@@ -154,8 +226,7 @@ subroutine  writexmlspecies
   call xml_endElement (xf, "basis")
 
   call xml_AddComment(xf," Exciting code version: "//version//" ")
-  call xml_AddComment(xf," Description of method: "//trim(suffix)//" ")
-  call xml_AddComment(xf," "//trim(apwdescr)//" ")
+  call xml_AddComment(xf," Generation Scheme: "//trim(apwdescr)//" ")
   call xml_Close(xf)
 
 end subroutine writexmlspecies
