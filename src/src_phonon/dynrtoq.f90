5b431753938497a891212d589bdf6626adf18c70
Stefan Kontur <stefan.kontur@physik.hu-berlin.de>
2012-12-17 16:09:13 +0100

    Improved interpolation algorithm for phonons

--- src/src_phonon/dynrtoq.f90
+++ src/src_phonon/dynrtoq.f90
@@ -15,25 +15,42 @@ Subroutine dynrtoq (vpl, dynr, dynp)
      & ngridq(1)*ngridq(2)*ngridq(3))
       Complex (8), Intent (Out) :: dynp (3*natmtot, 3*natmtot)
 ! local variables
-      Integer :: i1, i2, i3, ir, i, j
-      Real (8) :: t1
-      Complex (8) zt1
+      Integer :: i1, i2, i3, ir, i, j, is, js, ia, ja, ii,jj
+      Real (8) :: vrl(3), vrsl(3)
+      Complex (8) :: zwght
+      Complex (8) :: dynr_ij(3,3), dynp_ij(3,3)
+!
       dynp (:, :) = 0.d0
+! loop over atoms and species
+      i = 1
+      Do is = 1,nspecies
+         Do ia = 1,natoms(is)
+            j = 1
+            Do js = 1,nspecies
+               Do ja = 1,natoms(js)
 ! loop over R-vectors
-      ir = 0
-      Do i3 = ngridq (3) / 2 - ngridq (3) + 1, ngridq (3) / 2
-         Do i2 = ngridq (2) / 2 - ngridq (2) + 1, ngridq (2) / 2
-            Do i1 = ngridq (1) / 2 - ngridq (1) + 1, ngridq (1) / 2
-               ir = ir + 1
-               t1 = - twopi * &
-              & (vpl(1)*dble(i1)+vpl(2)*dble(i2)+vpl(3)*dble(i3))
-               zt1 = cmplx (Cos(t1), Sin(t1), 8)
-               Do i = 1, 3 * natmtot
-                  Do j = 1, 3 * natmtot
-                     dynp (i, j) = dynp (i, j) + zt1 * dynr (i, j, ir)
+                  ir = 0
+                  dynp_ij(:,:) = (0.d0, 0.d0)
+                  Do i3 = ngridq (3) / 2 - ngridq (3) + 1, ngridq (3) / 2
+                     Do i2 = ngridq (2) / 2 - ngridq (2) + 1, ngridq (2) / 2
+                        Do i1 = ngridq (1) / 2 - ngridq (1) + 1, ngridq (1) / 2
+                           ir = ir + 1
+                           dynr_ij(:,:) = dynr(i:i+2,j:j+2,ir)
+                           vrl(1) = dble(i1)
+                           vrl(2) = dble(i2)
+                           vrl(3) = dble(i3)
+                           vrsl(:) = vrl(:) + input%structure%speciesarray(js)%species%atomarray(ja)%atom%coord(:) &
+                                           & - input%structure%speciesarray(is)%species%atomarray(ia)%atom%coord(:)
+                           call ws_weight(vrl, vrsl, vpl, zwght)
+                           dynp_ij(:,:) = dynp_ij(:,:) + zwght*dynr_ij(:,:)
+                        End Do
+                     End Do
                   End Do
+                  dynp(i:i+2,j:j+2) = dynp_ij(:,:)
+                  j = j + 3
                End Do
             End Do
+            i = i + 3
          End Do
       End Do
 ! symmetrise the dynamical matrix
