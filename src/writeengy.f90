f8aec0bf0c965eb8fdaea6413ed1000b845cc20f
Dmitrii Nabok <dmitrii.nabok@gmail.com>
2013-07-03 15:26:34 +0200

    Modifications from Rostam related to output of energy contributions. Rearrangments in the list of supported platforms. IFORT is now related to the latest version (XE) including MKL library. Other platforms still needs to be tested.

--- src/writeengy.f90
+++ src/writeengy.f90
@@ -10,42 +10,87 @@ Subroutine writeengy (fnum)
       Use modmain
       Use modinput
       Implicit None
-! arguments
+      ! arguments
       Integer, Intent (In) :: fnum
-      Write (fnum,*)
-      Write (fnum, '("Energies :")')
-      Write (fnum, '(" Fermi", T30, ": ", G22.12)') efermi
-      Write (fnum, '(" sum of eigenvalues", T30, ": ", G22.12)') &
-     & evalsum
-      Write (fnum, '(" electronic kinetic", T30, ": ", G22.12)') engykn
-      Write (fnum, '(" core electron kinetic", T30, ": ", G22.12)') &
-     & engykncr
-      Write (fnum, '(" Coulomb", T30, ": ", G22.12)') engycl
-      Write (fnum, '(" Coulomb potential", T30, ": ", G22.12)') engyvcl
-      Write (fnum, '(" nuclear-nuclear", T30, ": ", G22.12)') engynn
-      Write (fnum, '(" electron-nuclear", T30, ": ", G22.12)') engyen
-      Write (fnum, '(" Hartree", T30, ": ", G22.12)') engyhar
-      Write (fnum, '(" Madelung", T30, ": ", G22.12)') engymad
-      If (input%groundstate%chgexs .Ne. 0.d0) Then
-         Write (fnum, '(" comp. background charge", T30, ": ", G22.12)') engycbc
+      CHARACTER(LEN=6)     :: input_groundstate_verbosity
+
+      input_groundstate_verbosity = 'high'
+      If ( input_groundstate_verbosity .eq. 'low'    .or. &
+         & input_groundstate_verbosity .eq. 'normal' .or. &
+         & input_groundstate_verbosity .eq. 'high') Then
+         write (fnum,*)
+         write (fnum, '("% START ENERGY BLOCK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")')
+         write (fnum,*)
+         write (fnum, '("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")')
+         write (fnum, '("  Fermi energy", T45, ": ", F22.12)') efermi
+         write (fnum, '("  Total energy", T45, ": ", F22.12)') engytot
+         write (fnum, '("--------------------------------------------------------------------")')
       End If
-      Write (fnum, '(" xc potential", T30, ": ", G22.12)') engyvxc
-      If (associated(input%groundstate%spin)) Then
-         Write (fnum, '(" xc effective B-field", T30, ": ", G22.12)') &
-        & engybxc
-         Write (fnum, '(" external B-field", T30, ": ", G22.12)') &
-        & engybext
+      If ( input_groundstate_verbosity .eq. 'normal' .or. &
+         & input_groundstate_verbosity .eq. 'high') Then
+         write (fnum,*)
+         write (fnum, '("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")')
+         write (fnum, '("Contributions to the total energy")')
+         write (fnum, '("--------------------------------------------------------------------")')
+         write (fnum, '("  Kinetic energy"                        , T45, ": ", F22.12)') engykn
+         write (fnum, '("  Coulomb energy"                        , T45, ": ", F22.12)') engycl
+         Write (fnum, '("  Exchange energy"                       , T45, ": ", F22.12)') engyx
+         Write (fnum, '("  Correlation energy"                    , T45, ": ", F22.12)') engyc
+         If (input%groundstate%chgexs .Ne. 0) Then
+            write (fnum, '("  Correction to the background charge", T45, ": ", F22.12)') engycbc
+         End If
+         If(engylu .Ne. 0) then
+            write (fnum, '("  LDA+U correction"                   , T45, ": ", F22.12)') engylu
+         End if
+         If (associated(input%groundstate%spin)) Then
+            write (fnum, '("(External B-field energy excluded from the total energy)")')
+         End If
+         write (fnum, '("--------------------------------------------------------------------")')
       End If
-      Write (fnum, '(" exchange", T30, ": ", G22.12)') engyx
-      Write (fnum, '(" correlation", T30, ": ", G22.12)') engyc
-      If (ldapu .Ne. 0) Then
-         Write (fnum, '(" LDA+U", T30, ": ", G22.12)') engylu
+      If (input_groundstate_verbosity .eq. 'high') Then
+         write (fnum,*)
+         write (fnum, '("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")')
+         write (fnum, '("The kinetic energy is the difference of the following two terms")')
+         write (fnum, '("--------------------------------------------------------------------")')
+         write (fnum, '("  Sum of eigenvalues"        , T45, ": ", F22.12)') evalsum
+         write (fnum, '("  Effective potential energy", T45, ": ", F22.12)') &
+         & engyvcl + engyvxc + engybxc + engybext + engybmt
+         write (fnum, '("--------------------------------------------------------------------")')
+
+         write (fnum,*)
+         write (fnum, '("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")')
+         write (fnum, '("Contributions to the effective potential energy")')
+         write (fnum, '("--------------------------------------------------------------------")')
+         write (fnum, '("  Coulomb potential energy"                 , T45, ": ", F22.12)') engyvcl
+         write (fnum, '("  xc potential energy"                      , T45, ": ", F22.12)') engyvxc
+         If (associated(input%groundstate%spin)) Then
+            Write (fnum, '("  xc effective B-field energy"           , T45, ": ", F22.12)') engybxc
+            Write (fnum, '("  External B-field energy"               , T45, ": ", F22.12)') engybext
+         End If
+         If(engybmt .Ne. 0) then
+            write (fnum, '("  Non-physical muffin-tin B-field energy", T45, ": ", F22.12)') engybmt
+         End If
+         write (fnum, '("--------------------------------------------------------------------")')
+
+         write (fnum,*)
+         write (fnum, '("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")')
+         write (fnum, '("Contributions to the Coulomb energy")')
+         write (fnum, '("--------------------------------------------------------------------")')
+         write (fnum, '("  Hartree energy"         , T45, ": ", F22.12)') engyhar
+         write (fnum, '("  Electron-nuclear energy", T45, ": ", F22.12)') engyen
+         write (fnum, '("  Nuclear-nuclear energy" , T45, ": ", F22.12)') engynn
+         write (fnum, '("--------------------------------------------------------------------")')
+
+         write (fnum,*)
+         write (fnum, '("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")')
+         write (fnum, '("Further energies")')
+         write (fnum, '("--------------------------------------------------------------------")')
+         Write (fnum, '("  Madelung energy"             , T45, ": ", F22.12)') engymad
+         Write (fnum, '("  Core-electron kinetic energy", T45, ": ", F22.12)') engykncr
+         write (fnum, '("--------------------------------------------------------------------")')
       End If
-      Write (fnum, '(" total energy", T30, ": ", G22.12)') engytot
-      If (associated(input%groundstate%spin)) Then
-         Write (fnum, '(" (external B-field energy excluded from total)&
-        &")')
-      End If
-!call flushifc(fnum)
+      write (fnum,*)
+      write (fnum, '("% END ENERGY BLOCK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")')
+      !call flushifc(fnum)
       Return
 End Subroutine
