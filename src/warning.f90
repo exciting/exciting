b37fdae4b5b2cdc387133833b2830b3744712e05
Dmitrii Nabok <dmitrii.nabok@gmail.com>
2013-08-29 10:00:09 +0200

    Fixed the bug connected with printing to fort.100 some information related to INFOXS.OUT

--- src/warning.f90
+++ src/warning.f90
@@ -2,15 +2,18 @@ subroutine warning(message)
 #ifdef MPI
     use modmpi
 #endif
-    
+    use m_getunit
+        
     implicit none
     character :: message*(*)
+    integer   :: unit
 #ifdef MPI
     if (rank==0) then
 #endif
-      open(100,File='WARNINGS.OUT',Action='WRITE',Position='APPEND')
-      write(100,*) trim(message)
-      close(100)
+      call getunit(unit)
+      open(unit,File='WARNINGS.OUT',Action='WRITE',Position='APPEND')
+      write(unit,*) trim(message)
+      close(unit)
 #ifdef MPI
     end if
 #endif
@@ -23,16 +26,19 @@ subroutine delete_warnings()
 #ifdef MPI
     use modmpi
 #endif
+    use m_getunit
 
     implicit none
+    integer   :: unit
     logical :: exist
 #ifdef MPI    
     if (rank==0) then
 #endif
       inquire (File='WARNINGS.OUT', Exist=exist)
       if (exist) Then
-        open(100, File='WARNINGS.OUT')
-        close(100, Status='DELETE')
+        call getunit(unit)
+        open(unit, File='WARNINGS.OUT')
+        close(unit, Status='DELETE')
       end if
 #ifdef MPI
     end if
