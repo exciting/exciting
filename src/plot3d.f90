6e1e6ab17a9f5270eb37abb6413967b339aa0eef
Dmitrii Nabok <dmitrii.nabok@gmail.com>
2014-02-11 14:22:33 +0100

    Bug in gengrid is fixed. plot3d is now fully functional (hopefully)

--- src/plot3d.f90
+++ src/plot3d.f90
@@ -32,7 +32,7 @@ Subroutine plot3d (plotlabels3d, nf, lmax, ld, rfmt, rfir, plotdef)
 ! !REVISION HISTORY:
 !   Created June 2003 (JKD)
 !   Modified, October 2008 (F. Bultmark, F. Cricchio, L. Nordstrom)
-!   Modified, February 2011 (D. Nabok)
+!   Modified, February 2011 (DIN)
 !   Bug fixing, May 2012 (DIN)
 !EOP
 !BOC
@@ -70,9 +70,9 @@ Subroutine plot3d (plotlabels3d, nf, lmax, ld, rfmt, rfir, plotdef)
 ! allocate the grid point arrays
 !
         allocate(ipmap(0:plotdef%box%grid(1),&
-       &  0:plotdef%box%grid(2), 0:plotdef%box%grid(3)))
+        &  0:plotdef%box%grid(2), 0:plotdef%box%grid(3)))
         allocate(vpl(3,(plotdef%box%grid(1)+1)* &
-       &  (plotdef%box%grid(2)+1)*(plotdef%box%grid(3)+1)))
+        &  (plotdef%box%grid(2)+1)*(plotdef%box%grid(3)+1)))
 !
 ! generate the 3d point grid and reduce it using the crystal symmetry
 !
@@ -214,41 +214,42 @@ Subroutine gengrid (ngridp, npt, ipmap, vpl)
 ! local variables
       Integer :: i1, i2, i3, ip, jp
       Integer :: isym, lspl
+      integer :: iv(3)
       Real (8) :: v1(3), v2(3), v3(3)
       Real (8) :: s(3,3), t1
 
-!------------------------------------------------------------------
+      !-----------------------------------------------------------------
 
       ip = 0
       Do i3 = 0, ngridp(3)
-         v1 (3) = dble(i3) / dble(ngridp(3))
-         Do i2 = 0, ngridp (2)
-            v1 (2) = dble(i2) / dble(ngridp(2))
+         v1(3) = dble(i3)/dble(ngridp(3))
+         Do i2 = 0, ngridp(2)
+            v1(2) = dble(i2)/dble(ngridp(2))
             Do i1 = 0, ngridp(1)
-               v1 (1) = dble(i1) / dble(ngridp(1))
-! determine if this point is equivalent to one already in the set
+               v1(1) = dble(i1)/dble(ngridp(1))
+               ! determine if this point is equivalent to one already in the set
                Do isym = 1, nsymcrys
-                  v2(:) = v2(:)+vtlsymc(:,isym)
+                  v2(:) = v1(:)+vtlsymc(:,isym)+plotdef%box%origin%coord(:)
+                  Call r3frac(input%structure%epslat,v2,iv)
                   lspl = lsplsymc (isym)
                   s(:,:) = dble(symlatc(:,:,lspl))
                   Call r3mv(s,v2,v3)
-                  !Call r3frac (input%structure%epslat, v3, iv)
+                  Call r3frac(input%structure%epslat,v3,iv)
                   Do jp = 1, ip
                      t1 = Abs(vpl(1,jp)-v3(1)) + &
                     &     Abs(vpl(2,jp)-v3(2)) + &
                     &     Abs(vpl(3,jp)-v3(3))
                      If (t1 .Lt. input%structure%epslat) Then
-! equivalent point found
+                        ! equivalent point found
                         ipmap(i1,i2,i3) = jp
-                        Go To 10
+                        goto 10
                      End If
                   End Do
                End Do
-! add new point to set
+               ! add new point to set
                ip = ip+1
                ipmap(i1,i2,i3) = ip
-               !Call r3frac (input%structure%epslat, v1, iv)
-               vpl(:,ip) = v1(:)
+               vpl(:,ip) = v2(:)
 10             Continue
             End Do
          End Do
