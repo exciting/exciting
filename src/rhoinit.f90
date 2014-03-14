a1ebc976d2735563614c99564c2e45952971f8d0
Andris Gulans <gulans@physik.hu-berlin.de>
2014-03-14 11:48:24 +0100

    Optional extra smoothness to the initial density is available on demand.

--- src/rhoinit.f90
+++ src/rhoinit.f90
@@ -47,7 +47,7 @@ Subroutine rhoinit
       Complex (8), Allocatable :: zfft (:)
 
       integer :: auxgridsize,mtgridsize,lastpoint
-      Real (8), Allocatable :: auxgrid(:),auxrho(:),a(:),c(:)
+      Real (8), Allocatable :: auxgrid(:),auxrho(:),a(:),c(:),b(:)
       Real (8) :: rhoder,rhoder2, tp(2),r
       real(8), parameter :: threshold=1d-12
       integer, parameter :: PointsPerPeriod=20
@@ -80,6 +80,7 @@ Subroutine rhoinit
 !      Allocate (z2fmt(lmmax, nrcmtmax))
       Allocate (zfft(ngrtot))
       allocate(a(nspecies))
+      allocate(b(nspecies))
       allocate(c(nspecies))
 
 ! zero the charge density and magnetisation arrays
@@ -122,15 +123,33 @@ Subroutine rhoinit
          Call fderiv (1, spnr(is), spr(:, is), sprho (:, is), gr, cf)
          rhoder=cf(1,nrmt(is))
          rhoder2=cf(2,nrmt(is))*2d0
+
+! pick .false. if you need extra smoothness 
+if (.true.) then 
+!-------- quadratic function
          a(is)=rhoder/(2d0*rmt(is))
          c(is)=sprho (nrmt(is), is)-a(is)*rmt(is)**2
 ! Interior of MT is filled with a*r**2+c
          do ir = 1, mtgridsize
            auxrho(ir)=a(is)*auxgrid(ir)**2+c(is)
          enddo
+else
+!-------- biquadratic function
+         a(is)=(rhoder2*rmt(is)-rhoder)/(8d0*rmt(is)**3)
+         b(is)=(3d0*rhoder-rhoder2*rmt(is))/(4d0*rmt(is))
+         c(is)=sprho (nrmt(is), is)-a(is)*rmt(is)**4-b(is)*rmt(is)**2
+! Interior of MT is filled with a*r**2+c
+         do ir = 1, mtgridsize
+           auxrho(ir)=a(is)*auxgrid(ir)**4+b(is)*auxgrid(ir)**2+c(is)
+         enddo
+endif
+
+
 ! Exterior is filled with the actual atomic density.
          auxrho(mtgridsize+1:auxgridsize)=sprho(nrmt(is)+1:lastpoint, is)
-
+!do ir=1,auxgridsize
+!  write(*,*) auxgrid(ir),auxrho(ir)
+!enddo
 
 ! Initialise auxiliary basis - spherical Bessel functions
          nsw=int(2d0*input%groundstate%gmaxvr*auxgrid(auxgridsize)/(pi))+1
@@ -681,7 +700,7 @@ endif
       Call charge
 ! normalise the density
       Call rhonorm
-      Deallocate (ffacg, zfmt, zfft,a,c,rhomodel)
+      Deallocate (ffacg, zfmt, zfft,a,b,c,rhomodel)
       call timesec(tb)
       write(*,*) 'rhoinit, step 3:',tb-ta
 !      stop
