e6dee816b166643baf3e1d0fa7624640e8898d6a
Santiago Rigamonti <santiago.rigamonti@physik.hu-berlin.de>
2014-06-11 14:01:32 +0200

    add subroutine for the verification of input for stm

---
+++ src/src_stm/checkinput.F90
@@ -0,0 +1,48 @@
+!
+!
+! Copyright (C) 2014 S. Rigamonti and C. Ambrosch-Draxl.
+! This file is distributed under the terms of the GNU General Public License.
+! See the file COPYING for license details.
+!
+!
+Subroutine checkinput()
+  Use modmain
+  Use modinput
+  use modplotlabels
+
+  ! local variables
+  Integer :: i, count, iz
+  Real(8) :: xc,yc,zc
+
+  If(associated(input%properties%stm%plane)) Then
+     count = 0
+     Do i = 1, 3
+        zc = input%structure%crystal%basevect(3, i)
+        If (zc.gt.input%structure%epslat) Then
+           count=count+1
+           iz = i
+        End If
+     End do
+     If (count .Gt. 1) Then
+        write(*,*)
+        write(*,'("Error(stm): More than one lattice vector with nonzero z component detected.")')
+        write(*,'("When using stm/plane element, it is assumed that")')
+        write(*,'("only one lattice vector has nonzero z component. Stopping")')
+        write(*,*)
+        stop
+     End If
+     xc =  input%structure%crystal%basevect(1, iz)
+     yc =  input%structure%crystal%basevect(1, iz)
+     If ((xc.gt.input%structure%epslat).or.(yc.gt.input%structure%epslat)) Then
+        write(*,*)
+        write(*,'("Error(stm): Lattice vector fo z component has non-null x or y component.")')
+        write(*,'("When using stm/plane element, it is assumed that")')
+        write(*,'("the lattice vector defining the z component is orthogonal to the xy plane. Stopping")')
+        write(*,*)
+        stop
+     End If
+  End If
+End Subroutine checkinput
+
+
+
