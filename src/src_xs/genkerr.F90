76876bbc644a171b345fcc5ac789e372551d53ba
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2014-07-07 16:50:49 +0200

    select lower branch of sqrt

--- src/src_xs/genkerr.F90
+++ src/src_xs/genkerr.F90
@@ -17,10 +17,25 @@ Contains
     Complex (8), Intent (Out) :: kerr (:)
     ! local variables
     Character (*), Parameter :: thisnam = 'genkerr'
-    ! optical conductivity
+    Complex(8), Allocatable :: sqeps11(:)
+    Integer :: n1 (1), n, iw, sig
+    n1 = shape (w)
+    n = n1 (1)
+         
+    allocate(sqeps11(n))
 
-    kerr (:) = -eps(1,2,:)/((eps(1,1,:)-1.0d0) * Sqrt(eps(1,1,:)))
+    !select lower branch of sqrt
+    Do iw = 2, n
+       sqeps11(iw) = sqrt(eps(1,1,iw))
+       If (aimag(sqeps11(iw)).gt.0.0d0) Then
+          sqeps11(iw) = -sqeps11(iw)
+       End If
+    End Do
+    sqeps11(1) = cmplx(1.0d0,0.0d0,8)
+    
+    kerr (:) = -eps(1,2,:)/((eps(1,1,:)-1.0d0) * sqeps11(:))
 
+    deallocate(sqeps11)
   End Subroutine genkerr
   !
 End Module m_genkerr
