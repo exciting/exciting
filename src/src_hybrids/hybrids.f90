c07f662555882b0f4793dedf3d2878d2ec928353
toxa81 <toxa81@gmail.com>
2015-04-28 16:51:13 +0200

    no need to compare boolean variable with .true. or .false.

--- src/src_hybrids/hybrids.f90
+++ src/src_hybrids/hybrids.f90
@@ -215,7 +215,7 @@ Subroutine hybrids
 ! calculate the non-local potential
 !-----------------------------------
         call timesec(ts0)
-        if ((ihyb==0).and.(restart==.true.)) then
+        if ((ihyb==0).and.restart) then
           Call getvnlmat 
           call timesec(ts1)
           if (rank==0) then
