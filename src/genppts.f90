a0c53c26cb73983aff892360385c0cf0a75c8129
Ute Werner <ute.werner@physik.hu-berlin.de>
2016-02-12 15:06:55 +0100

    small bug fix

--- src/genppts.f90
+++ src/genppts.f90
@@ -143,7 +143,7 @@ subroutine genppts (reducep, tfbz, ngridp, boxl, nppt, ipmap, &
         v1(1) = dble(i1) / dble(ngridp(1))
         do i2 = 0, ngridp(2)-1
           v1(2) = dble(i2) / dble(ngridp(2))
-            do i3 = 0, ngridp(1)-1
+            do i3 = 0, ngridp(3)-1
             v1(3) = dble(i3) / dble(ngridp(3))
             call r3mv(b, v1, v2)
             v2(:) = v2(:)+boxl(:,1)
