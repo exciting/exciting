664b47d39c328acd845b7501f2977cb944738121
Dmitrii Nabok <dnabok@dune2.(none)>
2016-06-23 11:13:13 +0200

    Changes in bandstructure.dat and bandstructure-qp.dat output formatting

--- src/src_gw/task_band.f90
+++ src/src_gw/task_band.f90
@@ -11,7 +11,7 @@ subroutine task_band()
   integer(4)    :: ik, ib, ib0
   real(8)       :: tstart, tend
   integer       :: i, j
-  character(80) :: fname
+  character(80) :: fname, s
   logical       :: exist
 
   !------------------------
@@ -30,15 +30,15 @@ subroutine task_band()
   end if
 
   open(70, File='bandstructure.dat', Action='Read', Status='Old')
-  read(70,*) ib0, nstsv, nkpt
-  write(*,*) ib0, nstsv, nkpt
+  read(70,*) s, ib0, nstsv, nkpt
+  write(*,*) trim(s), ib0, nstsv, nkpt
   if (allocated(vkl)) deallocate(vkl)
   allocate(vkl(3,nkpt))
   if (allocated(dpp1d)) deallocate(dpp1d)
   allocate(dpp1d(nkpt))
   if (allocated(evalsv)) deallocate(evalsv)
   allocate(evalsv(nstsv,nkpt))
-  do ib = 1, nstsv
+  do ib = ib0, nstsv
     do ik = 1, nkpt
       ! Note: evalsv are already shifted to E_f = 0
       read(70,*) i, j, vkl(:,ik), dpp1d(ik), evalsv(ib,ik)
@@ -57,6 +57,7 @@ subroutine task_band()
   !----------------------------------
   open(50, File='BAND-QP.OUT', Action='Write', Form='Formatted')
   open(51, File="bandstructure-qp.dat", Action='Write', Form='Formatted')
+  write(51,*) "# ", ibgw, min(nbgw,nstsv), nkpt
   do ib = ibgw, min(nbgw,nstsv)
     do ik = 1, nkpt
       ! old format (gwmod-boron) 
