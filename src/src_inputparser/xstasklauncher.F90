5ed8369b86bd20430f7cd6458eeaa744edfea59c
Benjamin Aurich <aurich@physik.hu-berlin.de>
2016-08-01 20:20:57 +0200

    Added a beyond switch and a corresponding if clause in the tasklauncher.

--- src/src_inputparser/xstasklauncher.F90
+++ src/src_inputparser/xstasklauncher.F90
@@ -1,334 +1,437 @@
-
 ! Copyright (C) 2009-2010 S. Sagmeister, C. Meisenbichler and C. Ambrosch-Draxl.
 ! This file is distributed under the terms of the GNU General Public License.
 ! See the file COPYING for license details.
 
-Subroutine xstasklauncher
-      Use modinput
-      Use modmain, Only: task
-      Use modxs, only: dgrid, nksubpt, iksubpt, temat, doscreen0, vkloff_xs_b,hybridhf,skipgnd
-      Use inputdom
+subroutine xstasklauncher
+  use modinput
+  use modmain, only: task
+  use modxs, only: dgrid, nksubpt, iksubpt, temat, doscreen0, vkloff_xs_b,hybridhf,skipgnd
+  use inputdom
 
-!
-      If ( .Not. (associated(input%xs%tddft))) Then
-  ! set the default values if tddft element not present
-         input%xs%tddft => getstructtddft (emptynode)
-      End If
-!
-      If ( .Not. (associated(input%xs%screening))) Then
-  ! set the default values if screening element not present
-         input%xs%screening => getstructscreening (emptynode)
-      End If
-!
-      If ( .Not. (associated(input%xs%BSE))) Then
-  ! set the default values if BSE element not present
-         input%xs%BSE => getstructBSE (emptynode)
-      End If
-!
-      If ( .Not. (associated(input%xs%tetra))) Then
-  ! set the default values if tetra element not present
-         input%xs%tetra => getstructtetra (emptynode)
-      End If
- ! if set to true KS eingenvalues and eigenvectors are not recalculated
-      skipgnd=input%xs%skipgnd
-      If (associated(input%groundstate%Hybrid)) Then
-        If (input%groundstate%Hybrid%exchangetypenumber == 1) Then
-            hybridhf = .True.
-            skipgnd = .True.
-        End If
-      End If    
- 
-!
-      Call backup0
-      Call backup1
-      Call backup2
-!
-      If (associated(input%xs%plan)) Then
-         Call xsmain (input%xs%plan)
-      Else If (trim(input%xs%xstype) .Eq. "TDDFT") Then
-!
-         If (input%xs%tddft%do .eq. "fromkernel") Go To 10
-!
-         task = 301
-         Call xsinit
-         Call xsgeneigvec
-         Call xsfinit
-!
-         If ((input%xs%tetra%tetradf)) Then
+  ! SET DEFAULTS
+  ! Set the default values if "tddft" element is not present
+  if( .not. associated(input%xs%tddft) ) then
+    input%xs%tddft => getstructtddft(emptynode)
+  end if
+
+  ! Set the default values if "screening" element is not present
+  if( .not. associated(input%xs%screening) ) then
+    input%xs%screening => getstructscreening(emptynode)
+  end if
+
+  ! Set the default values if "bse" element is not present
+  if( .not. associated(input%xs%bse) ) then
+    input%xs%bse => getstructbse(emptynode)
+  end if
+
+  ! Set the default values if "tetra" element not present
+  if( .not. associated(input%xs%tetra) ) then
+    input%xs%tetra => getstructtetra(emptynode)
+  end if
+
+  ! If "skipgnd" is set to true ks eigenvalues and eigenvectors are not recalculated
+  skipgnd=input%xs%skipgnd
+
+  if(associated(input%groundstate%hybrid)) then
+    if(input%groundstate%hybrid%exchangetypenumber == 1) then
+      hybridhf = .true.
+      skipgnd = .true.
+    end if
+  end if    
+
+  call backup0
+  call backup1
+  call backup2
+
+  if(associated(input%xs%plan)) then
+
+    call xsmain(input%xs%plan)
+
+  else if(trim(input%xs%xstype) .eq. "TDDFT") then
+
+    if(input%xs%tddft%do .eq. "fromkernel") go to 10
+
+    task = 301
+    call xsinit
+    call xsgeneigvec
+    call xsfinit
+
+    if(input%xs%tetra%tetradf) then
 #ifdef TETRA         
-            task = 310
-            Call xsinit
-            Call tetcalccw
-            Call xsfinit
+      task = 310
+      call xsinit
+      call tetcalccw
+      call xsfinit
 #else
-            ! added by DIN
-            write(*,*) 'Tetrahedron method for XS is disabled!'
-            write(*,*) 'Check -DTETRA option in make.inc' 
-            stop
+      ! added by din
+      write(*,*) 'tetrahedron method for xs is disabled!'
+      write(*,*) 'check -dtetra option in make.inc' 
+      stop
 #endif            
-         End If
-!
-         task = 320
-         Call xsinit
-         Call writepmatxs
-         Call xsfinit
-!
-         task = 330
-         Call xsinit
-         If(temat) Then
-            Call writeemat
-         End If
-         Call xsfinit
-!
-         If (input%xs%tddft%fxctypenumber .Eq. 7 .Or. &
-        & input%xs%tddft%fxctypenumber .Eq. 8) Then
-            task = 401
-            Call xsinit
-            Call scrgeneigvec
-            Call xsfinit
-!
-            task = 420
-            Call xsinit
-            Call scrwritepmat
-            Call xsfinit
-!
-            If ((input%xs%tetra%tetradf)) Then
+    end if
+
+    task = 320
+    call xsinit
+    call writepmatxs
+    call xsfinit
+
+    task = 330
+    call xsinit
+    if(temat) then
+      call writeemat
+    end if
+    call xsfinit
+
+    ! What is fxctypenumber ? - Benjmain Aurich 2016-08-01
+    if(input%xs%tddft%fxctypenumber .eq. 7 .or. &
+       & input%xs%tddft%fxctypenumber .eq. 8) then
+      task = 400
+      call xsinit
+      call scrgeneigvec
+      call xsfinit
+
+      task = 420
+      call xsinit
+      call scrwritepmat
+      call xsfinit
+
+      if ((input%xs%tetra%tetradf)) then
 #ifdef TETRA            
-               task = 410
-               Call xsinit
-               Call scrtetcalccw
-               Call xsfinit
+        task = 410
+        call xsinit
+        call scrtetcalccw
+        call xsfinit
 #else
-            ! added by DIN
-            write(*,*) 'Tetrahedron method for XS is disabled!'
-            write(*,*) 'Check -DTETRA option in make.inc' 
-            stop
+        ! added by din
+        write(*,*) 'tetrahedron method for xs is disabled!'
+        write(*,*) 'check -dtetra option in make.inc' 
+        stop
 #endif
-            End If
-!
-            If (input%xs%screening%do .Eq. "fromscratch") Then
-               task = 430
-               Call xsinit
-               Call screen
-               Call xsfinit
-!
-               task = 440
-               Call xsinit
-               Call scrcoulint
-               Call xsfinit
-            End If
-!
-            task = 450
-            Call xsinit
-            Call kernxc_bse
-            Call xsfinit
-         End If
-!
-         task = 340
-         Call xsinit
-         Call df
-         Call xsfinit
-!
-10       Continue
-!
-         task = 350
-         Call xsinit
-         Call idf
-         Call xsfinit
-!
-      Else If ((trim(input%xs%xstype)=="BSE") .AND. (.not.input%xs%BSE%xas)) Then
-!
-! STK
-! apply double grid technique if requested
-       if (any(input%xs%BSE%ngridksub .gt. 1)) then
-          dgrid = .true.
-       else
-          dgrid = .false.
-          nksubpt = 1
-       endif
-       if (dgrid) then
-          ! append XS output
-          input%xs%tappinfo = .true.
-          ! backup input XS vkloff (it will be added to all generated grids)
-          vkloff_xs_b(:) = input%xs%vkloff(:)
-          ! save screening status for later use
-          doscreen0 = input%xs%screening%do
-          ! generate subgrid
-          call genksubpts
-       endif
-       do iksubpt = 1, nksubpt
-         if (dgrid) call bsedgridinit
-!
-         task = 301
-         Call xsinit
-         Call xsgeneigvec
-         Call xsfinit
-!
-         task = 320
-         Call xsinit
-         Call writepmatxs
-         Call xsfinit
-!
-         task = 401
-         Call xsinit
-         Call scrgeneigvec
-         Call xsfinit
-!
-         If ((input%xs%tetra%tetradf)) Then
+      end if
+
+      if (input%xs%screening%do .eq. "fromscratch") then
+        task = 430
+        call xsinit
+        call screen
+        call xsfinit
+
+        task = 440
+        call xsinit
+        call scrcoulint
+        call xsfinit
+      end if
+
+      task = 450
+      call xsinit
+      call kernxc_bse
+      call xsfinit
+
+    end if
+
+    task = 340
+    call xsinit
+    call df
+    call xsfinit
+
+10  continue
+
+    task = 350
+    call xsinit
+    call idf
+    call xsfinit
+
+  else if(trim(input%xs%xstype)=="BSE" .and. (.not. input%xs%bse%xas)) then
+
+    ! STK
+    ! Apply double grid technique if requested
+    if(any(input%xs%bse%ngridksub .gt. 1)) then
+      dgrid = .true.
+    else
+      dgrid = .false.
+      nksubpt = 1
+    endif
+
+    if(dgrid) then
+      ! append xs output
+      input%xs%tappinfo = .true.
+      ! backup input xs vkloff (it will be added to all generated grids)
+      vkloff_xs_b(:) = input%xs%vkloff(:)
+      ! save screening status for later use
+      doscreen0 = input%xs%screening%do
+      ! generate subgrid
+      call genksubpts
+    endif
+
+    do iksubpt = 1, nksubpt
+
+      if(dgrid) call bsedgridinit
+
+      task = 301
+      call xsinit
+      call xsgeneigvec
+      call xsfinit
+
+      task = 320
+      call xsinit
+      call writepmatxs
+      call xsfinit
+
+      task = 401
+      call xsinit
+      call scrgeneigvec
+      call xsfinit
+
+      if((input%xs%tetra%tetradf)) then
 #ifdef TETRA            
-            task = 410
-            Call xsinit
-            Call scrtetcalccw
-            Call xsfinit
+        task = 410
+        call xsinit
+        call scrtetcalccw
+        call xsfinit
 #else
-            ! added by DIN
-            write(*,*) 'Tetrahedron method for XS is disabled!'
-            write(*,*) 'Check -DTETRA option in make.inc' 
-            stop
+        ! added by din
+        write(*,*) 'tetrahedron method for xs is disabled!'
+        write(*,*) 'check -dtetra option in make.inc' 
+        stop
 #endif
-         End If
-!
-         task = 420
-         Call xsinit
-         Call scrwritepmat
-         Call xsfinit
-!
-         If (input%xs%screening%do .Eq. "fromscratch") Then
-            task = 430
-            Call xsinit
-            Call screen
-            Call xsfinit
-!
-            task = 440
-            Call xsinit
-            Call scrcoulint
-            Call xsfinit
-         End If
-!
-         task = 441
-         Call xsinit
-         Call exccoulint
-         Call xsfinit
-!
-         task = 445
-         Call xsinit
-         Call BSE
-         Call xsfinit
-!
-         if (dgrid) input%xs%screening%do = "skip"
-!
-       enddo
-!
-       if (dgrid) then
-          call bsedgrid
-          ! restore input settings
-          input%xs%vkloff(:) = vkloff_xs_b(:)
-          input%xs%screening%do = doscreen0
-       endif
-!
-      Else If ((trim(input%xs%xstype)=="BSE") .AND. (input%xs%BSE%xas)) Then
-!
-! STK
-! apply double grid technique if requested
-       if (any(input%xs%BSE%ngridksub .gt. 1)) then
-          dgrid = .true.
-       else
-          dgrid = .false.
-          nksubpt = 1
-       endif
-       if (dgrid) then
-          ! append XS output
-          input%xs%tappinfo = .true.
-          ! backup input XS vkloff (it will be added to all generated grids)
-          vkloff_xs_b(:) = input%xs%vkloff(:)
-          ! save screening status for later use
-          doscreen0 = input%xs%screening%do
-          ! generate subgrid
-          call genksubpts
-       endif
-       do iksubpt = 1, nksubpt
-         if (dgrid) call bsedgridinit
-!
-         task = 301
-         Call xsinit
-         Call xsgeneigvec
-         Call xsfinit
-!
-         task = 320
-         Call xsinit
-         Call xasinit
-         Call writepmatxs
-         Call xasfinit
-         Call xsfinit
-!
-         task = 401
-         Call xsinit
-         Call scrgeneigvec
-         Call xsfinit
-!
-         If ((input%xs%tetra%tetradf)) Then
+      end if
+
+      task = 420
+      call xsinit
+      call scrwritepmat
+      call xsfinit
+
+      if(input%xs%screening%do .eq. "fromscratch") then
+        task = 430
+        call xsinit
+        call screen
+        call xsfinit
+
+        task = 440
+        call xsinit
+        call scrcoulint
+        call xsfinit
+      end if
+
+      task = 441
+      call xsinit
+      call exccoulint
+      call xsfinit
+
+      task = 445
+      call xsinit
+      call bse
+      call xsfinit
+
+      if(dgrid) input%xs%screening%do = "skip"
+
+    enddo
+
+    if (dgrid) then
+       call bsedgrid
+       ! restore input settings
+       input%xs%vkloff(:) = vkloff_xs_b(:)
+       input%xs%screening%do = doscreen0
+    endif
+
+  else if(trim(input%xs%xstype)=="BSE" .and. input%xs%bse%xas) then
+
+    ! STK
+    ! Apply double grid technique if requested
+    if (any(input%xs%bse%ngridksub .gt. 1)) then
+      dgrid = .true.
+    else
+      dgrid = .false.
+      nksubpt = 1
+    endif
+
+    if(dgrid) then
+      ! append xs output
+      input%xs%tappinfo = .true.
+      ! backup input xs vkloff (it will be added to all generated grids)
+      vkloff_xs_b(:) = input%xs%vkloff(:)
+      ! save screening status for later use
+      doscreen0 = input%xs%screening%do
+      ! generate subgrid
+      call genksubpts
+    endif
+
+    do iksubpt = 1, nksubpt
+      if(dgrid) call bsedgridinit
+
+      task = 301
+      call xsinit
+      call xsgeneigvec
+      call xsfinit
+
+      task = 320
+      call xsinit
+      call xasinit
+      call writepmatxs
+      call xasfinit
+      call xsfinit
+
+      task = 401
+      call xsinit
+      call scrgeneigvec
+      call xsfinit
+
+      if ((input%xs%tetra%tetradf)) then
 #ifdef TETRA            
-            task = 410
-            Call xsinit
-            Call scrtetcalccw
-            Call xsfinit
+        task = 410
+        call xsinit
+        call scrtetcalccw
+        call xsfinit
 #else
-            ! added by DIN
-            write(*,*) 'Tetrahedron method for XS is disabled!'
-            write(*,*) 'Check -DTETRA option in make.inc' 
-            stop
+        ! added by din
+        write(*,*) 'tetrahedron method for xs is disabled!'
+        write(*,*) 'check -dtetra option in make.inc' 
+        stop
 #endif
-       End If
-!
-         task = 420
-         Call xsinit
-         Call scrwritepmat
-         Call xsfinit
-!
-         If (input%xs%screening%do .Eq. "fromscratch") Then
-            task = 430
-            Call xsinit
-            Call screen
-            Call xsfinit
-!
-            task = 440
-            Call xsinit
-            Call xasinit
-            Call xas_scrcoulint
-            Call xasfinit
-            Call xsfinit
-         End If
-!
-         task = 441
-         Call xsinit
-         Call xasinit
-         Call xas_exccoulint
-         Call xasfinit
-         Call xsfinit
-!
-         task = 445
-         Call xsinit
-         Call xasinit
-         Call xas
-         Call xasfinit
-         Call xsfinit
-!
-         if (dgrid) input%xs%screening%do = "skip"
-!
-       enddo
-!
-       if (dgrid) then
-          call bsedgrid
-          ! restore input settings
-          input%xs%vkloff(:) = vkloff_xs_b(:)
-          input%xs%screening%do = doscreen0
-       endif
-!
-      Else
-         Write (*,*) "error xstasklauncher"
-         Write (*,*) trim (input%xs%xstype), "no valid xstype"
-         Stop
-      End If
-!
-!
-End Subroutine
+      end if
+
+      task = 420
+      call xsinit
+      call scrwritepmat
+      call xsfinit
+
+      if (input%xs%screening%do .eq. "fromscratch") then
+        task = 430
+        call xsinit
+        call screen
+        call xsfinit
+
+        task = 440
+        call xsinit
+        call xasinit
+        call xas_scrcoulint
+        call xasfinit
+        call xsfinit
+      end if
+
+      task = 441
+      call xsinit
+      call xasinit
+      call xas_exccoulint
+      call xasfinit
+      call xsfinit
+
+      task = 445
+      call xsinit
+      call xasinit
+      call xas
+      call xasfinit
+      call xsfinit
+
+      if (dgrid) input%xs%screening%do = "skip"
+
+    enddo
+
+    if (dgrid) then
+      call bsedgrid
+      ! restore input settings
+      input%xs%vkloff(:) = vkloff_xs_b(:)
+      input%xs%screening%do = doscreen0
+    endif
+
+  else if(trim(input%xs%xstype)=="BSE" .and. input%xs%bse%beyond) then
+
+    ! STK
+    ! Apply double grid technique if requested
+    if(any(input%xs%bse%ngridksub .gt. 1)) then
+      dgrid = .true.
+    else
+      dgrid = .false.
+      nksubpt = 1
+    endif
+
+    if(dgrid) then
+      ! append xs output
+      input%xs%tappinfo = .true.
+      ! backup input xs vkloff (it will be added to all generated grids)
+      vkloff_xs_b(:) = input%xs%vkloff(:)
+      ! save screening status for later use
+      doscreen0 = input%xs%screening%do
+      ! generate subgrid
+      call genksubpts
+    endif
+
+    do iksubpt = 1, nksubpt
+
+      if(dgrid) call bsedgridinit
+
+      task = 301
+      call xsinit
+      call xsgeneigvec
+      call xsfinit
+
+      task = 320
+      call xsinit
+      call writepmatxs
+      call xsfinit
+
+      task = 401
+      call xsinit
+      call scrgeneigvec
+      call xsfinit
+
+      if((input%xs%tetra%tetradf)) then
+#ifdef TETRA            
+        task = 410
+        call xsinit
+        call scrtetcalccw
+        call xsfinit
+#else
+        ! added by din
+        write(*,*) 'tetrahedron method for xs is disabled!'
+        write(*,*) 'check -dtetra option in make.inc' 
+        stop
+#endif
+      end if
+
+      task = 420
+      call xsinit
+      call scrwritepmat
+      call xsfinit
+
+      if(input%xs%screening%do .eq. "fromscratch") then
+        task = 430
+        call xsinit
+        call screen
+        call xsfinit
+
+        task = 440
+        call xsinit
+        call scrcoulint
+        call xsfinit
+      end if
+
+      task = 441
+      call xsinit
+      call exccoulint
+      call xsfinit
+
+      task = 445
+      call xsinit
+      call bse
+      call xsfinit
+
+      if(dgrid) input%xs%screening%do = "skip"
+
+    enddo
+
+    if (dgrid) then
+       call bsedgrid
+       ! restore input settings
+       input%xs%vkloff(:) = vkloff_xs_b(:)
+       input%xs%screening%do = doscreen0
+    endif
+
+  else
+
+     write (*,*) "error xstasklauncher"
+     write (*,*) trim (input%xs%xstype), "no valid xstype"
+     stop
+
+  end if
+
+end subroutine
