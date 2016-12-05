04aeadf5e41626c3680fee2f75365936b7e2b458
Benjamin Aurich <aurich@physik.hu-berlin.de>
2016-12-05 23:50:03 +0100

    TI+Scalapack runable

---
+++ src/src_xs/m_invertzmat.f90
@@ -0,0 +1,170 @@
+module m_invertzmat
+  use modmpi 
+  use modscl
+
+  implicit none
+
+  private
+
+  public :: zinvert, dzinvert
+
+  contains
+
+    !BOP
+    ! !ROUTINE: zinvert
+    ! !INTERFACE:
+    subroutine zinvert(zmat)
+    ! !INPUT/OUTPUT PARAMETERS:
+    ! IN/OUT:
+    !   complex(8) :: zmat  ! On entry: Matrix.
+    !                       ! On exit : Inverted matrix.
+    !
+    ! !DESCRIPTION:
+    !   Inverts complex matrix.
+    ! 
+    ! !REVISION HISTORY:
+    !   Created 2016 (Aurich)
+    !EOP
+    !BOC
+
+      ! Arguments
+      complex(8), intent(inout) :: zmat(:,:)
+
+      integer(4) :: m, n
+      integer(4) :: info, lwork
+      integer(4), allocatable :: ipiv(:)
+      complex(8), allocatable :: work(:)
+
+      m = size(zmat,1)
+      n = size(zmat,2)
+
+      if(m /= n) then 
+        write(*,*) "Error(zivert): m /= n"
+        call terminate
+      end if
+
+      allocate(ipiv(m))
+
+      ! LU factorization of general complex matrix A
+      call zgetrf(m, n, zmat, m, ipiv, info)
+      if(info /= 0) then
+        write(*,'("Error(zinvert): ZGETRF has returned non-zero info.")')
+        if(info < 0) then
+          write(*,'("  ZGETRF: Incorrect input.")')
+        else
+          write(*,'("  ZGETRF: U is exactly singular.")')
+        end if
+        call terminate
+      end if
+      
+      ! Compute Inverse with LU factorized A
+
+      ! Workspace query 
+      lwork = -1
+      allocate(work(3))
+      call zgetri(m, zmat, m, ipiv, work, lwork, info)
+      lwork = int(work(1))
+      deallocate(work)
+      allocate(work(lwork))
+
+      ! Computation
+      call zgetri(m, zmat, m, ipiv, work, lwork, info)
+      if(info /= 0) then
+        write(*,'("Error(zinvert): ZGETRI has returned non-zero info.")')
+        if(info < 0) then
+          write(*,'("  ZGETRF: Incorrect input.")')
+        else
+          write(*,'("  ZGETRF: Matrix singular.")')
+        end if
+        call terminate
+      end if
+      deallocate(work)
+      deallocate(ipiv)
+
+    end subroutine zinvert
+    !EOC
+
+    !BOP
+    ! !ROUTINE: dzinvert
+    ! !INTERFACE:
+    subroutine dzinvert(zmat)
+    ! !INPUT/OUTPUT PARAMETERS:
+    ! IN/OUT:
+    !   type(dzmat) :: zmat ! On entry: Distributed matrix to invert.
+    !                       ! On exit : Inverted matrix.
+    !
+    ! !DESCRIPTION:
+    !   Takes a distributed general complex matrix and inverts it.
+    ! 
+    ! !REVISION HISTORY:
+    !   Created 2016 (Aurich)
+    !EOP
+    !BOC
+
+      implicit none
+
+      ! Arguments
+      type(dzmat), intent(inout) :: zmat
+
+      integer(4) :: info
+      integer(4) :: ia, ja
+      integer(4) :: lwork, liwork
+      integer(4), allocatable :: ipiv(:)
+      integer(4), allocatable :: iwork(:)
+      complex(8), allocatable :: work(:)
+
+      if(zmat%isdistributed == .false.) then 
+        call zinvert(zmat%za)
+        return
+      end if
+
+#ifdef SCAL
+      allocate(ipiv(zmat%nrows_loc+zmat%mblck))
+
+      ! LU factorization of general complex matrix A
+      ia = 1
+      ja = 1
+      call pzgetrf(zmat%nrows, zmat%ncols, zmat%za, ia, ja, zmat%desc,&
+        & ipiv, info)
+      if(info /= 0) then
+        write(*,'("Error(dzinvert): PZGETRF has returned non-zero info.")')
+        if(info < 0) then
+          write(*,'("  PZGETRF: Incorrect input.")')
+        else
+          write(*,'("  PZGETRF: U is exactly singular.")')
+        end if
+        call terminate
+      end if
+      
+      ! Compute Inverse with LU factorized A
+
+      ! Workspace query 
+      liwork = -1
+      allocate(iwork(3))
+      allocate(work(3))
+      call pzgetri(zmat%nrows, zmat%za, ia, ja, zmat%desc, ipiv,&
+        & work, lwork, iwork, liwork, info)
+      lwork = int(work(1))
+      liwork = iwork(1)
+      deallocate(work,iwork)
+      allocate(work(lwork),iwork(liwork))
+
+      ! Computation
+      call pzgetri(zmat%nrows, zmat%za, ia, ja, zmat%desc, ipiv,&
+        & work, lwork, iwork, liwork, info)
+      if(info /= 0) then
+        write(*,'("Error(dzinvert): PZGETRI has returned non-zero info.")')
+        if(info < 0) then
+          write(*,'("  PZGETRF: Incorrect input.")')
+        else
+          write(*,'("  PZGETRF: Matrix singular.")')
+        end if
+        call terminate
+      end if
+      deallocate(work,iwork)
+      deallocate(ipiv)
+#endif
+    end subroutine dzinvert
+    !EOC
+
+end module m_invertzmat
---
+++ src/src_xs/m_sqrtzmat.f90
@@ -0,0 +1,129 @@
+module m_sqrtzmat
+
+  use modmpi
+  use modscl
+  use m_dhesolver
+  use m_dzgemm
+
+  implicit none
+
+  private
+
+  complex(8), parameter :: zone = (1.0d0, 0.0d0)
+  complex(8), parameter :: zzero = (0.0d0, 0.0d0)
+
+  public :: sqrtzmat_hepd, sqrtdzmat_hepd
+
+  contains
+
+    subroutine sqrtzmat_hepd(hepdmat)
+
+      complex(8), intent(inout) :: hepdmat(:,:)
+
+      integer(4) :: m, n, i, j
+      complex(8), allocatable :: evecs(:,:)
+      real(8), allocatable :: evals(:)
+
+      m = size(hepdmat,1)
+      n = size(hepdmat,2)
+
+      if(m /= n) then 
+        write(*,*) "Error(sqrtmat_hepd): m /= n"
+        call terminate
+      end if
+
+      allocate(evecs(m,m))
+      allocate(evals(m))
+
+      ! Diagonalize hermitian matrix
+      call hesolver(hepdmat, evecs, evals)
+
+      ! Take square root of eigenvalues
+      if(any(evals < 0.0d0)) then 
+        write(*,*) "Error(sqrtmat_hepd): Matrix is not positive definit"
+        write(*,'(E10.3)') evals
+        call terminate
+      else
+        ! Take D^{1/4} for: A^1/2 = Q D^1/2 Q^H = (Q D^1/4)*(Q D^1/4)^H
+        evals = sqrt(sqrt(evals))
+      end if
+
+      ! Make Q D^1/4
+      do j = 1, m
+        do i = 1, m
+          evecs(i,j) = evecs(i,j)*evals(j)
+        end do
+      end do
+      
+      ! Construct square root matrix
+      call zgemm('N','C', m, m, m, zone, evecs, m,&
+        & evecs, m, zzero, hepdmat, m)
+
+      deallocate(evecs)
+      deallocate(evals)
+
+    end subroutine sqrtzmat_hepd
+
+    subroutine sqrtdzmat_hepd(hepdmat, binfo)
+
+      type(dzmat), intent(inout) :: hepdmat
+      type(blacsinfo), intent(in) :: binfo
+
+      integer(4) :: m, n, i, j, jg
+      type(dzmat) :: evecs
+      real(8), allocatable :: evals(:)
+
+      if(hepdmat%isdistributed == .false.) then
+        call sqrtzmat_hepd(hepdmat%za)
+        return
+      end if
+
+      if(hepdmat%context /= binfo%context) then
+        write(*,*) "Error(sqrtdzmat_hepd): Wrong BLACS context."
+        write(*,*) "  mat%context =", hepdmat%context
+        write(*,*) "  binfo%context =", binfo%context
+        call terminate
+      end if
+
+      m = hepdmat%nrows
+      n = hepdmat%ncols
+
+      if(m /= n) then 
+        write(*,*) "Error(sqrtdzmat_hepd): m /= n"
+        call terminate
+      end if
+
+      allocate(evals(m))
+
+      call new_dzmat(evecs, m, n, binfo, hepdmat%mblck, hepdmat%nblck)
+
+      ! Diagonalize hermitian matrix
+      call dhesolver(hepdmat, evecs, evals, binfo)
+
+      ! Take square root of eigenvalues
+      if(any(evals < 0.0d0)) then 
+        write(*,*) "Error(sqrtmat_hepd): Matrix is not positive definit"
+        write(*,'(E10.3)') evals
+        call terminate
+      else
+        ! Take D^{1/4} for: A^1/2 = Q D^1/2 Q^H = (Q D^1/4)*(Q D^1/4)^H
+        evals = sqrt(sqrt(evals))
+      end if
+
+      ! Make Q D^1/4
+      do j = 1, evecs%ncols_loc
+        jg = evecs%c2g(j)
+        do i = 1, evecs%nrows_loc
+          evecs%za(i,j) = evecs%za(i,j)*evals(jg)
+        end do
+      end do
+      
+      ! Construct square root matrix
+      call dzgemm(evecs, evecs, hepdmat, transb='C')
+
+      deallocate(evals)
+      call del_dzmat(evecs)
+
+    end subroutine sqrtdzmat_hepd
+
+end module m_sqrtzmat
