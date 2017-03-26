db9232cb57018d86f8b8a816643d9c96381ead82
Benjamin Aurich <aurich@physik.hu-berlin.de>
2017-03-26 22:51:26 +0200

    added simple progessbar

--- src/src_xs/b_scrcoulint.f90
+++ src/src_xs/b_scrcoulint.f90
@@ -882,11 +882,11 @@ use m_writecmplxparts
     deallocate(igqmap)
     deallocate(wfc)
 
-    !if(rank == 0) then
-    !  write(6, '(a,"Scrcoulint progess:", f10.3)', advance="no")&
-    !    & achar( 13), 100.0d0*dble(ikkp-ppari+1)/dble(pparf-ppari+1)
-    !  flush(6)
-    !end if
+    if(rank == 0) then
+      write(6, '(a,"Scrcoulint progess:", f10.3)', advance="no")&
+        & achar( 13), 100.0d0*dble(ikkp-ppari+1)/dble(pparf-ppari+1)
+      flush(6)
+    end if
 
   ! End loop over(k,kp)-pairs
   end do kkploop
