7e54f57a2e8dc2665b18acc0c4f2db86029fc5a1
Maria Troppenz <mariat@physik.hu-berlin.de>
2017-06-08 09:29:09 +0200

    kubo file with boltzmann and some changes for kubo transport

--- src/src_optics/kubo.f90
+++ src/src_optics/kubo.f90
@@ -10,9 +10,17 @@ subroutine kubo
     integer :: ik, jk, isym
     integer :: ist, jst, iw
     integer :: recl, iostat
+    integer :: mugrid, tempgrid
+    integer :: n
     logical :: exist, intraband, drude
-    real(8) :: wplas, swidth, eji, t1, t2
+    real(8) :: wplas, swidth, eji, t1, t2, t3
     real(8) :: v1 (3), v2 (3), sc(3,3)
+    real(8) :: kb
+    real(8) :: deltaw
+    real(8) :: tempi,tempf,temps,temp
+    real(8) :: mui, muf, mus, mu, deltamu
+    complex(8) :: sigmaboltz(9)
+    complex(8) :: sigmak(9)
     complex(8) :: zt1, zt2
     character(256) :: fname
 ! allocatable arrays
@@ -20,12 +28,20 @@ subroutine kubo
     real(8), allocatable :: evalsvt(:), occsvt(:)
     complex (8), allocatable :: pmat(:,:,:)
     complex (8), allocatable :: sigma(:)
+    complex (8), allocatable :: sigmab(:)
+    complex (8), allocatable :: seebeck(:)
+    complex (8), allocatable :: thermalcond(:)
+    complex (8), allocatable :: zt(:)
+    complex (8), allocatable :: td(:) ! Transport distribution function
     complex (8), allocatable :: eta(:)
     integer :: wgrid
     integer :: kfirst, klast
     real(8) :: wmax, scissor
 ! external functions
     real(8) sdelta
+
+    kb =  0.30036326d-5 ! Boltzmann constant Ha/K
+
     
     if (rank==0) then
       write(*,*)
@@ -41,17 +57,30 @@ subroutine kubo
 
 ! working arrays
     wgrid = input%properties%dielmat%wgrid
+    mugrid = input%properties%dielmat%mugrid
+    tempgrid = input%properties%dielmat%tempgrid
+
+!    write(*,*) wgrid
+!    write(*,*) mugrid
+!    write(*,*) tempgrid
+    
     allocate(w(wgrid))
+    allocate(td(wgrid))
     allocate(sigma(wgrid))
+    allocate(sigmab(tempgrid*mugrid))
+    allocate(seebeck(tempgrid*mugrid))
+    allocate(thermalcond(tempgrid*mugrid))
+    allocate(zt(tempgrid*mugrid))
 
 ! generate energy grid
-    wmax = input%properties%dielmat%wmax
-    t1 = wmax/dble(wgrid)
+    !wmax = input%properties%dielmat%wmax
+    deltaw = input%properties%dielmat%deltaw
+    
+    t1 = deltaw/(dble(wgrid))
     Do iw = 1, wgrid
-        w(iw) = t1*dble(iw-1)
+        w(iw) = t1*(iw-1)-deltaw/(1.0*2)+efermi
     End Do
 
-    write(*,*) wmax
 ! if element "drude" present, use the specified parameters for the Drude term 
 ! instead of calculating the plasma frequency
     drude = .false.
@@ -133,6 +162,33 @@ subroutine kubo
         end do
     end if
 
+! initialize sigmaboltz to zeros
+    sigmaboltz(:) = zzero
+    sigmak(:) = zzero
+
+    tempi=input%properties%dielmat%temprange(1)
+    tempf=input%properties%dielmat%temprange(2)
+    if (input%properties%dielmat%tempgrid == 1) then
+       temps=1
+    else
+       temps=(tempf-tempi)/(input%properties%dielmat%tempgrid-1)
+    end if
+    
+    deltamu=input%properties%dielmat%deltamu
+    if (input%properties%dielmat%mugrid == 1) then
+       mus=1
+    else
+       mus=(deltamu)/(input%properties%dielmat%mugrid-1)
+    end if
+
+    if (mus == 1) then
+       mui=efermi
+       muf=efermi+0.5
+    else
+       mui=efermi-deltamu/2.0
+       muf=efermi+deltamu/2.0
+    end if
+
     do l = 1, ncomp
         
         a = epscomp(1,l)
@@ -143,6 +199,11 @@ subroutine kubo
 
 ! sum over non-reduced k-points
         sigma(:) = zzero
+        sigmab(:) = zzero
+        seebeck(:) = zzero
+        thermalcond(:) = zzero
+        zt(:) = zzero
+        td(:) = zzero
         wplas = 0.0d0
         do ik = kfirst, klast
 
@@ -180,6 +241,34 @@ subroutine kubo
             end if
 
 !--------------------------
+! Transport distribution
+!--------------------------       
+!--------------------------       
+            do ist = 1, nstsv
+               zt1 = pmat(a,ist,ist)*conjg(pmat(b,ist,ist))
+               do iw = 1, wgrid
+                  t1 = (evalsvt(ist)-w(iw))/input%groundstate%swidth
+                  !td(iw) = td(iw)+ zt1/input%properties%dielmat%drude(2)*sdelta(input%groundstate%stypenumber,t1)/input%groundstate%swidth
+                  !write(*,*) 1/input%properties%dielmat%drude(2)
+                  td(iw) = td(iw)+ zt1*sdelta(input%groundstate%stypenumber,t1)/input%groundstate%swidth
+
+               end do ! iw
+            end do ! ist
+
+!--------------------------
+! sigmak from Kubo
+!--------------------------       
+!--------------------------       
+            do ist = 1, nstsv
+               zt1 = pmat(a,ist,ist)*conjg(pmat(b,ist,ist))
+               t1 = (evalsvt(ist)-efermi)/(tempi*kb)
+               t2 = exp(t1)
+               t3 = t2/((t2+1)**2 * tempi*kb)
+               sigmak(l) = sigmak(l)+ zt1*t3
+            end do ! ist
+
+            
+!--------------------------
 ! INTERBAND CONTRIBUTION
 !--------------------------       
             do ist = 1, nstsv
@@ -216,13 +305,34 @@ subroutine kubo
             end do ! ist
             
         end do ! ik
-        
-        zt1 = zi/(omega*dble(nkptnr))
-        sigma(:) = zt1*sigma(:)
 
+
+        !do iw = 1, wgrid
+        !   t1 = (efermi-w(iw))/(input%properties%dielmat%t*kb)
+        !   sigmab(iw) = sigmab(iw)+ td(iw)*sdelta(3,t1)/(input%properties%dielmat%t*kb)
+           !sigmaboltz(l) = sigmaboltz(l) + td(iw)*sdelta(3,t1)/(input%properties%dielmat%t*kb)
+           !write (*,*) td(iw), sdelta(3,t1), t1
+        !end do ! iw
+        !zt1 = twopi**3/(omega*dble(nkptnr))
+        !t1 = deltaw/dble(wgrid)
+        !sigmab(:) = t1*zt1*sigmab(:)
+               
+        
+
+
+        !sigmab(:) = zt1*sigmab(:)
+        !sigmaboltz(l) = zt1*sigmaboltz(l)
+        
 #ifdef MPI
         call MPI_AllReduce(MPI_IN_PLACE, &
         &                  sigma, &
+        &                  wgrid, &
+        &                  MPI_DOUBLE_COMPLEX, &
+        &                  MPI_SUM, &
+        &                  MPI_COMM_WORLD, &
+        &                  ierr)
+        call MPI_AllReduce(MPI_IN_PLACE, &
+        &                  td, &
         &                  wgrid, &
         &                  MPI_DOUBLE_COMPLEX, &
         &                  MPI_SUM, &
@@ -239,12 +349,64 @@ subroutine kubo
         end if
         call MPI_Barrier(MPI_COMM_WORLD,ierr)
 #endif
+        if (rank==0) then
+        n=0
+        do mu = mui, muf, mus
+           
+           do temp = tempi, tempf, temps
 
+        !do temp = tempi, tempf, temps
+           !do mu = mui, muf, mus
+              n=n+1
+              !write(*,*) n
+              do iw = 1, wgrid
+                 t1 = -(mu-w(iw))/(temp*kb)
+                 t2 = exp(t1)
+                 t3 = t2/((t2+1)**2 * temp*kb)
+                 sigmab(n) = sigmab(n) + td(iw)*t3
+                 seebeck(n) = seebeck(n) + td(iw)*t3*t1
+                 thermalcond(n) = thermalcond(n) + td(iw)*t3*(t1**2)
+                 !sigmab(n) = sigmab(n) + td(iw)*sdelta(3,t1)/(temp*kb)
+                 !seebeck(n) = seebeck(n) + td(iw)*sdelta(3,t1)/(temp*kb)*(-1)*t1
+                 !thermalcond(n) = thermalcond(n) + td(iw)*sdelta(3,t1)/(temp*kb)*(t1**2)
+              !write (*,*) td(iw), sdelta(3,t1), t1, sigmaboltz(l), l
+              end do ! iw
+              zt1 = twopi**3/(omega*dble(nkptnr))
+              t1 = deltaw/dble(wgrid)
+              sigmab(n) = t1*zt1*sigmab(n) !/input%properties%dielmat%drude(2)
+              seebeck(n) = t1*zt1*kb*temp*seebeck(n)/sigmab(n)
+              thermalcond(n) = t1*zt1*(kb**2)*temp*temp*thermalcond(n)
+              zt(n) = sigmab(n)*seebeck(n)**2/(thermalcond(n))
+              !zt(n) = t1*zt1*(kb**2)*temp*temp*thermalcond(n)
+              !write(*,*) temp, mu, sigmab(n)
+
+           end do ! mu
+        end do ! temp
+
+        zt1 = zi/(omega*dble(nkptnr))
+        sigma(:) = zt1*sigma(:)
+
+        do iw = 1, wgrid
+           t1 = (efermi-w(iw))/(tempi*kb)
+           sigmaboltz(l) = sigmaboltz(l) + td(iw)*sdelta(3,t1)/(tempi*kb)
+           !write (*,*) td(iw), sdelta(3,t1), t1, sigmaboltz(l), l
+        end do ! iw
+        
+        zt1 = twopi**3/(omega*dble(nkptnr))
+        t1 = deltaw/dble(wgrid)
+        sigmaboltz(l) = t1*zt1*sigmaboltz(l)
+        write (*,*) sigmaboltz(l)
+
+        sigmak(l) = zt1*sigmak(l)
+        write (*,*) sigmak(l)
+
+        
 ! add the intraband contribution
         if (intraband) then
             if (drude) then
 ! use specified parameters for Drude term
                 zt1 = zi/fourpi
+
                 do iw = 1, wgrid
                     t1 = input%properties%dielmat%drude(1)
                     t2 = input%properties%dielmat%drude(2)
@@ -266,9 +428,10 @@ subroutine kubo
                     sigma(iw) = sigma(iw)+zt1*wplas/(w(iw)+eta(iw))
                 end do
             end if
-        end if        
+         end if
+         end if
 
-!-----------------------------------------------
+        !-----------------------------------------------
 ! Print the output spectra
 !-----------------------------------------------
         if (rank==0) then
@@ -278,11 +441,80 @@ subroutine kubo
 ! write the optical conductivity
             write(fname, '("SIGMA_", 2I1, ".OUT")') a, b
             write(*, '("  Optical conductivity tensor written to ", a)') trim(adjustl(fname))
+            write(*,*) trim(fname)
+            
+            open(61, file=trim(fname), action='WRITE', form='FORMATTED')
+            do iw = 1, wgrid
+                write(61, '(3G18.10)') t1*w(iw), sigma(iw)
+            end do
+            close(61)
+! write the transport distribution function from Boltzmann
+            write(fname, '("TD_", 2I1, ".OUT")') a, b
+            write(*, '("  Transport distribution written to ", a)') trim(adjustl(fname))
             open(60, file=trim(fname), action='WRITE', form='FORMATTED')
             do iw = 1, wgrid
-                write(60, '(3G18.10)') t1*w(iw), sigma(iw)
+                write(60, '(3G18.10)') t1*w(iw), td(iw)
             end do
            close(60)
+! write the optical conductivity from Boltzmann
+            write(fname, '("SIGMAB_", 2I1, ".OUT")') a, b
+            write(*, '("  Optical conductivity tensor written to ", a)') trim(adjustl(fname))
+            open(60, file=trim(fname), action='WRITE', form='FORMATTED')
+            n=0
+            do mu = mui, muf, mus
+               do temp = tempi, tempf, temps
+               !do mu = mui, muf, mus
+                  n=n+1
+                  write(60, '(4G18.10)') temp, mu, sigmab(n)
+               end do
+               write(60, ' ')
+            end do
+           close(60)
+! write the Seebeck coefficient from Boltzmann
+            write(fname, '("SEEBECK_", 2I1, ".OUT")') a, b
+            write(*, '("  Seebeck coefficient tensor written to ", a)') trim(adjustl(fname))
+            open(60, file=trim(fname), action='WRITE', form='FORMATTED')
+            n=0
+            do mu = mui, muf, mus
+               do temp = tempi, tempf, temps
+               !do mu = mui, muf, mus
+                  n=n+1
+                  write(60, '(4G18.10)') temp, mu, seebeck(n)
+               end do
+               write(60, ' ')
+            end do
+           close(60)
+! write the thermal conductivity from Boltzmann
+            write(fname, '("THERMALCOND_", 2I1, ".OUT")') a, b
+            write(*, '("  Thermal conductivity tensor written to ", a)') trim(adjustl(fname))
+            open(60, file=trim(fname), action='WRITE', form='FORMATTED')
+            n=0
+            do mu = mui, muf, mus
+               do temp = tempi, tempf, temps
+               !do mu = mui, muf, mus
+                  n=n+1
+                  write(60, '(4G18.10)') temp, mu, thermalcond(n)
+               end do
+               write(60, ' ')
+            end do
+           close(60)
+
+! write the figure of merit from Boltzmann
+            write(fname, '("ZT_", 2I1, ".OUT")') a, b
+            write(*, '("  Figure of merit zT written to ", a)') trim(adjustl(fname))
+            open(60, file=trim(fname), action='WRITE', form='FORMATTED')
+            n=0
+            do mu = mui, muf, mus
+               do temp = tempi, tempf, temps
+               !do mu = mui, muf, mus
+                  n=n+1
+                  write(60, '(4G18.10)') temp, mu, zt(n)
+               end do
+               write(60, ' ')
+            end do
+           close(60)
+
+           
 ! write the dielectric function to file
             write(fname, '("EPSILON_", 2I1, ".OUT")') a, b
             write(*, '("  Dielectric tensor written to ", a)') trim(adjustl(fname))
@@ -293,7 +525,7 @@ subroutine kubo
                 zt2 = t2+zt1*sigma(iw)/(w(iw)+eta(iw))
                 write(60, '(3G18.10)') t1*w(iw), zt2
             end do
-            close(60)
+           close(60)
 ! write the EELS spectra
             write(fname, '("LOSS_", 2I1, ".OUT")') a, b
             write(*, '("  Electron energy loss spectrum written to ", a)') trim(adjustl(fname))
@@ -304,7 +536,7 @@ subroutine kubo
                 zt2 = t2+zt1*sigma(iw)/(w(iw)+eta(iw))
                 write(60, '(2G18.10)') t1*w(iw), -aimag(1.0d0/zt2)
             end do
-            close(60)
+           close(60)
             if (input%properties%dielmat%tevout) write(*, '("  Output energy is in eV")')
             write(*,*)
         end if
@@ -313,10 +545,34 @@ subroutine kubo
 #endif
     
     end do ! l, optical components
-   
+
+
+    if (rank==0) then
+       
+    ! write the optical components of conductivity from Boltzmann     
+       write(fname, '("SIGMABOLTZ.OUT")')
+       write(*, '("  Optical conductivity components written to ", a)') trim(adjustl(fname))
+       open(60, file=trim(fname), action='WRITE', form='FORMATTED')
+       !write(*,*) sigmaboltz
+       do l = 1, ncomp
+          write(60, '(2I1,2G18.10)') epscomp(1,l), epscomp(2,l), sigmaboltz(l)
+       end do
+      close(60)
+    ! write the optical components of conductivity from Kubo
+       write(fname, '("SIGMAKUBO.OUT")')
+       write(*, '("  Optical conductivity components written to ", a)') trim(adjustl(fname))
+       open(60, file=trim(fname), action='WRITE', form='FORMATTED')
+       !write(*,*) sigmaboltz
+       do l = 1, ncomp
+          write(60, '(2I1,2G18.10)') epscomp(1,l), epscomp(2,l), sigmak(l)
+       end do
+      close(60)
+    end if
+    
     close(50) 
+
     deallocate(pmat)
     deallocate(evalsvt,occsvt)
-    deallocate(w,sigma,eta)
+    deallocate(w,sigma,sigmab,seebeck,thermalcond,zt,eta,td)
     
 end subroutine
