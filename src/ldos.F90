338c68c21bbbaab369f9bfb7cc29dbeffe36867d
Dmitrii Nabok <dmitrii.nabok@gmail.com>
2019-12-06 10:38:48 +0100

    Fixed a severe bug in LDOS

--- src/ldos.F90
+++ src/ldos.F90
@@ -6,12 +6,12 @@ subroutine ldos()
     use modinput
     use modmain,        only: ngrid, ngrtot, ngkmax, apwordmax, lmmaxapw, nmatmax, nrmtmax, nspinor, &
                               nstfv, nstsv, vkl, vgkl, evalsv, ngk, gkc, tpgkc, sfacgk, omega, nkpt, &
-                              ikmap, efermi, twopi, avec
+                              ikmap, efermi, twopi, avec, task
     use mod_rgrid
     use mod_xsf_format
     use mod_cube_format
     use modmpi
-    
+
     implicit none
     integer(4) :: ik, ib
     character(80) :: fname
@@ -22,7 +22,7 @@ subroutine ldos()
     complex(8), allocatable :: wfmt(:,:,:,:,:)
     complex(8), allocatable :: wfir(:,:,:)
     complex(8), allocatable :: zdata(:)
-    
+
     integer(4) :: i1, i2, i3, ip, np, nptot, npstep, i_dim
     integer(4) :: nw , iw, nsk(3), iw_f, iw_vb, iw_cb
     real(8) :: v0, dw, vbz
@@ -32,21 +32,18 @@ subroutine ldos()
     real(8), allocatable :: w(:)
     real(8), allocatable :: g(:)
     real(8), allocatable :: e(:,:)
-    
+
     type(rgrid) :: grid
     type(plot3d_type), pointer :: plot3d
-    ! Initialize global exciting variables
 
+    ! Initialize global exciting variables
     call init0
-    call init1    
-    ! read the density and potentials from file
+    call init1
     call readstate
-    ! find the new linearisation energies
+    call readfermi
     call linengy
-    ! generate the APW radial functions
     call genapwfr
-    ! generate the local-orbital radial functions
-    call genlofr
+    call genlofr(.False.)
 
     ! lengths of the unit cell basis vectors
     do i1 = 1, 3
@@ -90,30 +87,32 @@ subroutine ldos()
     if (delta > abc(3)) stop 'Error(ldos): Integration volume specified by delta is too big!'
     np = min(nint(abc(i_dim)/delta), nptot)
     allocate(weight(np, nstsv, nkpt))
+    weight(:,:,:) = 0.d0
     npstep = max(nptot/np, 1)
+
+    ! integration volume
+    v0 = omega / dble(grid%npt)
+
     ! print*, 'delta=', delta
     ! print*, 'nptot=', nptot
     ! print*, 'np=', np
     ! print*, 'npstep=', npstep
-
-    ! integration volume
-    v0 = omega / dble(grid%npt)
+    ! print*, 'v0=', v0
 
     !---------------------------------------
     ! begin parallel loop over k-points
     !---------------------------------------
     splittfile = .False.
 #ifdef MPI
-    call MPI_barrier(MPI_COMM_WORLD, ierr)
-    do ik = firstk(rank), lastk(rank)
+    call barrier()
+    do ik = firstofset(rank,nkpt), lastofset(rank,nkpt)
 #else
     do ik = 1, nkpt
 #endif
         ! get the eigenvectors and values from file
-        call getevalsv(vkl(:,ik), evalsv(:, ik))
         call getevecfv(vkl(:,ik), vgkl(:,:,:,ik), evecfv)
         call getevecsv(vkl(:,ik), evecsv)
-    
+
         ! compute the matching coefficients
         call match(ngk(1,ik), gkc(:,1,ik), tpgkc(:,:,1,ik), &
         &          sfacgk(:,:,1,ik), apwalm)
@@ -122,6 +121,7 @@ subroutine ldos()
 
             call genwfsv_new(ik, ib, ib, apwalm, evecfv, evecsv, wfmt, wfir)
             call calc_zdata_rgrid(grid, ik, wfmt(:,:,:,1,ib), wfir(:,1,ib), zdata)
+            ! write(*,*) 'zdata=', ik, ib, sum(zdata)
             ! if (rank==0) then
             !     write(fname,'("wf3d-",i4.4,"-",i4.4,".xsf")') ik, ib
             !     call str_strip(fname)
@@ -129,7 +129,7 @@ subroutine ldos()
             !     call write_3d_xsf(fname, 'module squared', grid%boxl(1:4,:), grid%ngrid, grid%npt, abs(zdata)**2)
             ! end if
 
-            ! real space partial density            
+            ! real space partial density
             ip = 0
             do i3 = 1, grid%ngrid(3)
             do i2 = 1, grid%ngrid(2)
@@ -139,6 +139,7 @@ subroutine ldos()
             end do
             end do
             end do
+            ! write(*,*) 'rho=', ik, ib, sum(rho)
 
             ! integral over the volume slice
             ip = 1
@@ -151,8 +152,8 @@ subroutine ldos()
 
     end do ! ik
 #ifdef MPI
-    call mpi_allgatherv_ifc(nkpt, np*nstsv, rbuf=weight)
-    call MPI_barrier(MPI_COMM_WORLD, ierr)
+    call mpi_allgatherv_ifc(nkpt, inplace=.False., rlen=np*nstsv, rbuf=weight)
+    call barrier()
 #endif
 
     ! memory cleaning
@@ -162,15 +163,15 @@ subroutine ldos()
     deallocate(evecfv)
     deallocate(evecsv)
     deallocate(wfmt, wfir)
-    
+
     !------------------------------------------------
     ! finally compute the local density of states
     !------------------------------------------------
     if (rank == 0) then
-   
-        call readfermi()
+
         allocate(e(nstsv,nkpt))
         do ik = 1, nkpt
+            call getevalsv(vkl(:,ik), evalsv(:, ik))
             do ib = 1, nstsv
                 ! subtract the Fermi energy
                 e(ib,ik) = evalsv(ib,ik)-efermi
@@ -179,7 +180,6 @@ subroutine ldos()
                     e(ib,ik) = e(ib,ik) + input%properties%ldos%scissor
             end do
         end do
-        deallocate(evalsv)
 
         ! generate energy grid
         nw = input%properties%ldos%nwdos
@@ -200,7 +200,7 @@ subroutine ldos()
 
         ! number of subdivisions used for interpolation
         nsk(:) = max(input%properties%ldos%ngrdos/input%groundstate%ngridk(:), 1)
-       
+
         open(30, file="ldos.out", action="write")
         open(31, file="band_edges.out", action="write")
         allocate(g(nw))
@@ -218,7 +218,7 @@ subroutine ldos()
             ! search for VBM and CBm
             !------------------------
             call find_vbm_cbm_1(iw_f, w, g, iw_vb, iw_cb)
-            ! call find_vbm_cbm_2(iw_f, w, g, iw_vb, iw_cb)            
+            ! call find_vbm_cbm_2(iw_f, w, g, iw_vb, iw_cb)
             ! output
             z = dble(ip-1)*abc(i_dim)/dble(np)
             write(31,'(3f16.6)') z, w(iw_vb), w(iw_cb)
@@ -226,7 +226,7 @@ subroutine ldos()
         close(30)
         close(31)
 
-        deallocate(g)       
+        deallocate(g)
         deallocate(w)
         deallocate(e)
         call delete_rgrid(grid)
--- test/test13/assert.pl
+++ test/test13/assert.pl
@@ -5,10 +5,19 @@ use XML::Writer;
 use IO::File;
 use Test;
 
-$tol = 0.1;
-
 $writer= Test::initreport("report.xml");
 
+$tol = 1.0;
+%statusse=Test::assert_file_same_within( "./reference/ldos.REF", "runldos/ldos.out", $tol);
+Test::writetestreport({
+ 		"directory"=>"test13/ ",
+ 		"name"=>"Local Density of States (ldos)",
+ 		"description"=>"The test is passed if the computed data differs
+ 		 less than ".sprintf("%.4f",$tol)." from the reference one:
+ 		 difference=".sprintf("%.4f",$statusse{"maxerror"}),
+ 		"status"=>$statusse{"status"}}, $writer);
+
+$tol = 0.1;
 %statusse=Test::assert_file_same_within( "./reference/band_edges.REF", "runldos/band_edges.out", $tol);
 Test::writetestreport({
  		"directory"=>"test13/ ",
