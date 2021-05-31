ad6c658fd576f9f28ab3bcd0f8d77908b81e24e0
Benedikt Moritz Maurer <benedikt.moritz.maurer@physik.hu-berlin.de>
2021-05-31 09:14:13 +0000

    Test suite/Run directory and reference data clean-up

--- test/tools/runner/clean.py
+++ test/tools/runner/clean.py
@@ -1,6 +1,8 @@
 import shutil
 import os
 
+from ..infrastructure import remove_ignored_files
+
 def clean_tests(test_farm:str, test_list:list, run_dir:str, ref_dir:str, ignored_output:list):
     """
     Cleans the tests in test_list (see clean_single_test).
@@ -13,7 +15,12 @@ def clean_tests(test_farm:str, test_list:list, run_dir:str, ref_dir:str, ignored
     print('Clean test directories.')
     
     for test_dir in test_list:
+        # remove run directory
         try:
             shutil.rmtree(os.path.join(test_farm, test_dir, run_dir))
         except Exception:
             pass
+
+        # clean up ref directory
+        remove_ignored_files(os.path.join(test_farm, test_dir, ref_dir), ignored_output)
+
