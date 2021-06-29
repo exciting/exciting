3ee7062ed3a04071207ec49c4a93cc39dfaadb5a
Alex Buccheri <alex.buccheri@physik.hu-berlin.de>
2021-06-29 10:52:58 +0000

    MR !104 adds dictionary utilities to excitingtools:
    * Added `get_hashable_entries`, which can be used to find all unique keys in parsed exciting data.
    
    * Added `serialise_dict_values`, which currently does not have a direct use case, however it could
      be used to refactor the test suite compare function (and has test coverage).
    
    * Added assertion messages to container_converter tests of dict_utils.

--- tools/exciting_tools/excitingtools/dict_utils.py
+++ tools/exciting_tools/excitingtools/dict_utils.py
@@ -1,9 +1,13 @@
-from typing import Union
-import json 
+"""
+Utilities for manipiulating dictionaries
+"""
+from typing import Callable, Iterator, Union
+import json
 import numpy as np
 import copy
+from collections.abc import MutableMapping, Mapping, Hashable
 import sys
-from collections.abc import Hashable
+
 
 def common_iterable(obj: Union[dict, list]):
     """
@@ -14,15 +18,15 @@ def common_iterable(obj: Union[dict, list]):
             print('index = key or integer index:', index)
             print('d[index] = dictionary value or list element:', d[index])
     """
-
     if isinstance(obj, dict):
         return obj
     else:
         return (index for index, value in enumerate(obj))
-    
-    
+
+
 def __container_converter(data: Union[list,dict]):
-    """Mutates the input dictionary, converting string representations of numerical data into numerical data.
+    """
+    Mutates the input dictionary, converting string representations of numerical data into numerical data.
 
     :param dict data: Dictionary with string values.
     :return:dict new_data: Dictionary with all string literal values converted to numerical values.
@@ -30,32 +34,94 @@ def __container_converter(data: Union[list,dict]):
     np_convert = {np.float64: float,
                   np.int32: int}
 
-    for element in common_iterable(data): 
+    for element in common_iterable(data):
         if isinstance(data[element], dict) or isinstance(data[element], list):
            data[element] = __container_converter(data[element])
         elif isinstance(data[element], np.ndarray):
             data[element] = data[element].tolist()
-        elif isinstance(data[element], (np.float64, np.int32)): 
+        elif isinstance(data[element], (np.float64, np.int32)):
             value = data[element]
             data[element] = np_convert[type(value)](value)
-        elif isinstance(data[element], Hashable): 
+        elif isinstance(data[element], Hashable):
             try:
                 data[element] = json.loads(data[element])
             except:
                 pass
-        else: 
+        else:
             message = 'Type not converted by dict converter: ' + str(type(data[element]))
             sys.exit(message)
-    
-    return data 
 
-   
+    return data
+
+
 def container_converter(data: dict):
-    """Mutates the input dictionary, converting string representations of numerical data into numerical data.
-    
+    """
+    Mutates the input dictionary, converting string representations of numerical data into numerical data.
+
     :param dict data: Dictionary to be copied containing string values.
     :return: dict new_data: Copied dictionary with all string literal values converted to numerical values.
-    """    
-    
+    """
+
     new_data = copy.deepcopy(data)
     return __container_converter(new_data)
+
+
+def serialise_dict_values(d):
+    """
+    Mutate dictionary values that are objects, to dictionaries.
+    Function works recursively, so no type-hinting.
+
+    If any value within the dictionary is a set, it is converted to a list because
+    common_iterable is not compatible with sets (sets cannot be indexed).
+
+    If any value within the dictionary is a tuple, it is converted to a list because
+    tuples are immutable, and therefore not consistent with an implementation that
+    mutates the input.
+
+    Sets and tuples are converted to lists regardless of whether they contain objects.
+
+    :param dict input_dict: Dictionary.
+    :param dict output_dict: Dictionary with any object values converted to dictionaries.
+    """
+
+    for index in common_iterable(d):
+        # Can't iterate over a set with an index, and tuple cannot be mutated
+        if isinstance(d[index], (set, tuple)):
+            d[index] = list(d[index])
+
+        # Class instances with the __dict__ attribute
+        if '__dict__' in dir(d[index]):
+            d[index] = vars(d[index])
+
+        # Recursively apply routine to other containers
+        elif not isinstance(d[index], Hashable):
+            d[index] = serialise_dict_values(d[index])
+
+        # pass over hashable values and np arrays
+        else:
+            pass
+
+    return d
+
+
+def get_hashable_entries(nested: dict) -> Iterator[tuple]:
+    """
+    Create a generator of all hashable values, removing nesting.
+
+    Note, this will fail if a cross edge or a back edge is included in the dict
+    i.e. a self-reference of the form:
+
+        A = {key1: value, key2: A}
+
+    However, for the use case of flattening parsed results, this should never be an issue.
+
+    Reference: https://stackoverflow.com/questions/10756427/loop-through-all-nested-dictionary-values
+
+    :param nested: Nested dictionary with a mix and hashable and non-hashable values.
+    :return: Iterator[tuple] (key, value): Generator for tuples, containing keys and values.
+    """
+    for key, value in nested.items():
+        if isinstance(value, abc.Mapping):
+            yield from get_hashable_entries(value)
+        else:
+            yield key, value
--- tools/exciting_tools/tests/test_dict_utils.py
+++ tools/exciting_tools/tests/test_dict_utils.py
@@ -1,32 +1,127 @@
-from excitingtools.dict_utils import container_converter
+"""
+Tests for functions in dict_utils.py
+"""
+import pytest
+import numpy as np
+from collections import Counter
+
+from excitingtools.dict_utils import container_converter, serialise_dict_values
+
 
 def test_convert_container():
-    data1 = {'a': '5.0', 'b': ['1.0', '10.0'], 'c': {'a1': '1.0'}, 'd': "blu", 'e': [['1', '2.3'], '3'], 'f': ['1', 'a']}
-    data2 = {'a': 5.0, 'b': [1.0, 10.0], 'c': {'a1': 1.0}, 'd': "blu", 'e': [[1, 2.3], 3], 'f': [1, 'a']}
+    input = {'a': '5.0', 'b': ['1.0', '10.0'], 'c': {'a1': '1.0'}, 'd': "blu", 'e': [['1', '2.3'], '3'],
+             'f': ['1', 'a']}
+    expected = {'a': 5.0, 'b': [1.0, 10.0], 'c': {'a1': 1.0}, 'd': "blu", 'e': [[1, 2.3], 3], 'f': [1, 'a']}
 
-    assert container_converter(data1) == data2
+    assert container_converter(input) == expected, 'String value/s failed to convert to numerical values'
+
 
 def test_convert_container_no_strings():
-    data1 = {'a': 5.0, 'b': [1.0, 10.0], 'c': {'a1': 1.0}, 'd': "blu", 'e': [[1, 2.3], 3], 'f': [1, 11.3]}
-    data2 = {'a': 5.0, 'b': [1.0, 10.0], 'c': {'a1': 1.0}, 'd': "blu", 'e': [[1, 2.3], 3], 'f': [1, 11.3]}
-    
-    assert container_converter(data1) == data2
+    input = {'a': 5.0, 'b': [1.0, 10.0], 'c': {'a1': 1.0}, 'd': "blu", 'e': [[1, 2.3], 3], 'f': [1, 11.3]}
+
+    assert container_converter(input) == input, "Expect the converter to do nothing"
+
 
 def test_convert_container_data_type():
-    data1 = {'a': '5.0', 'b': ['1.0', '10.0'], 'c': {'a1': '1.0'}, 'd': "blu", 'e': [['1', '2.3'], '3']}
-    data2 = {'a': 5.0, 'b': [1.0, 10.0], 'c': {'a1': 1.0}, 'd': "blu", 'e': [[1, 2.3], 3]}
+    input = {'a': '5.0', 'b': ['1.0', '10.0'], 'c': {'a1': '1.0'}, 'd': "blu", 'e': [['1', '2.3'], '3']}
+    expected = {'a': 5.0, 'b': [1.0, 10.0], 'c': {'a1': 1.0}, 'd': "blu", 'e': [[1, 2.3], 3]}
 
-    data1 = container_converter(data1)
-    
-    for elem1, elem2 in zip(data1.values(), data2.values()):
+    output = container_converter(input)
+
+    for elem1, elem2 in zip(output.values(), expected.values()):
         assert type(elem1) == type(elem2)
-    for elem, elem2 in zip(data1['c'].values(), data2['c'].values()): 
+    for elem, elem2 in zip(output['c'].values(), expected['c'].values()):
         assert type(elem) == type(elem2)
-    for elem, elem2 in zip(data1['b'], data2['b']): 
+    for elem, elem2 in zip(output['b'], expected['b']):
         assert type(elem) == type(elem2)
-    for elem, elem2 in zip(data1['e'], data2['e']): 
+    for elem, elem2 in zip(output['e'], expected['e']):
         assert type(elem) == type(elem2)
-    for elem, elem2 in zip(data1['e'][0], data2['e'][0]):
+    for elem, elem2 in zip(output['e'][0], expected['e'][0]):
         assert type(elem) == type(elem2)
 
-    
+    assert output == expected, "Output is consistent with the expected dictionary"
+
+
+class Mock:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+def test_serialise_dict_values():
+
+    # Value is an object
+    input = {'mock_key': Mock(1, 2)}
+    output = serialise_dict_values(input)
+    assert output == {'mock_key': {'a': 1, 'b': 2}}, "Convert object values to dicts"
+
+    # Object nested in a dictionary
+    input = {'mock_key': {'another-key': Mock(1, 2)}}
+    output = serialise_dict_values(input)
+    assert output == {'mock_key': {'another-key':{'a': 1, 'b': 2}}}, "Convert nested object values into dicts"
+
+    # Object nested in a list, and within a dictionary within a list
+    input = {'a': [1, 2, Mock(3, 4), {'b': Mock(5, 6)}]}
+    output = serialise_dict_values(input)
+    assert output == {'a': [1, 2, {'a': 3, 'b': 4}, {'b': {'a': 5, 'b': 6}}]}, \
+        "Convert nested object values into dicts, where the top-level container value is a list"
+
+
+def test_serialise_dict_value_is_tuple():
+
+    # Object nested in a dictionary, nested within a tuple
+    input = {'mock_key': (1, {'another-key': Mock(1, 2)})}
+    output = serialise_dict_values(input)
+    assert output == {'mock_key': [1, {'another-key': {'a': 1, 'b': 2}}]}, \
+        "Convert nested object values into dicts, where the top-level container value is a tuple." \
+        "Note, tuple cannot be mutated so it is converted to a list by serialise_dict_values"
+
+
+def test_serialise_dict_value_is_set():
+
+    # Object nested in a dictionary, nested within a set
+    input = { 'mock_key': {1, 2, Mock(1, 2)} }
+    output = serialise_dict_values(input)
+
+    output_keys = [k for k in output.keys()]
+    output_values = [v for v in output.values()]
+
+    assert len(output_keys) == 1
+    assert output_keys[0] == 'mock_key'
+    assert len(output_values) == 1
+
+    # Mock object -> dictionary, meaning one cannot compare the input and outout with collections.Counter,
+    # sets or sorted (neither object nor dictionary is hashable)
+    def unordered_lists_the_same(a: list, b: list) -> bool:
+        """
+        Check contents of two lists are consistent, irregardless of order
+        """
+        for element in a:
+            try:
+                b.remove(element)
+            except ValueError:
+                return False
+        return not b
+
+    assert unordered_lists_the_same(output_values[0], [1, 2, {'a': 1, 'b': 2}]), \
+        "Convert nested object values into dicts, where the top-level container value is a set. " \
+        "Note, set is not iterable and is converted to a list with some arbitrary order by serialise_dict_values"
+
+
+def test_serialise_dict_values_null_behaviour():
+
+    input = {'mock_key': [1, 2]}
+    output = serialise_dict_values(input)
+    assert output == input, "Pass over list values"
+
+    input = {'mock_key': (1, 2)}
+    output = serialise_dict_values(input)
+    assert output == input, "Pass over tuple values"
+
+    input = {'mock_key': {1, 2}}
+    output = serialise_dict_values(input)
+    assert output == input, "Pass over set values"
+
+    input = {'mock_key': np.array([1., 2., 3.])}
+    output = serialise_dict_values(input)
+    assert output == input, "Pass over np.array values"
