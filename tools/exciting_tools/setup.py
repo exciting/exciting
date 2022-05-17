069a7b5a209a2bd2d2cfe1dd2c7074987294c623
Alex Buccheri <alex.buccheri@physik.hu-berlin.de>
2022-05-17 18:58:06 +0200

    excitingtools.  Setup details and licence.
    * Add licence and missing setup details, such that excitingtools is PyPi-ready.

--- tools/exciting_tools/setup.py
+++ tools/exciting_tools/setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='excitingtools',
       version='0.0.1',
       description='Utilities for aiding in the construction of exciting inputs and the postprocessing exciting outputs.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='SOL Group',
+      url="http://exciting.wikidot.com",
       author_email='abuccheri@physik.hu-berlin.de',  # Add a point of contact
       packages=['excitingtools'],
       include_package_data=True,
+      python_requires=">=3.6",
       install_requires=['wheel>=0.35.0', 'numpy>=1.14.5','matplotlib>=2.2.0'],
       extras_requires=['ase=>3.20.0']
       )
