1bacdb880287a7db6001032b28c5683d41cd9aba
Maximilian Schebek <maximilian.schebek@physik.hu-berlin.de>
2022-06-27 13:10:04 +0200

    Transform BSE tutorial to Jupyter Notebook

--- tools/exciting_tools/excitingtools/parser/__init__.py
+++ tools/exciting_tools/excitingtools/parser/__init__.py
@@ -1,2 +1,3 @@
 from .parserChooser import parser_chooser
 from .erroneous_file_error import ErroneousFileError
+from .bse_parser import parse_EPSILON_NAR
--- tools/excitingjupyter/excitingjupyter/utilities.py
+++ tools/excitingjupyter/excitingjupyter/utilities.py
@@ -1,5 +1,7 @@
 import os
 import pathlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 def get_exciting_root() -> str:
     """Get exciting's root directory.
@@ -14,3 +16,33 @@ def get_exciting_root() -> str:
         excitingroot = os.environ['EXCITINGROOT']
     assert "exciting_smp" in os.listdir(os.path.join(excitingroot, "bin")), f"Exciting binary 'exciting_smp' was not found at exciting root path: {excitingroot}"
     return excitingroot
+
+# Modified from C. Vorwerk,
+# https://git.physik.hu-berlin.de/sol/toolbox/-/blob/master/notebooks/BSE/Example.ipynb
+def set_plot_parameters(figure_width: float, figure_height: float,
+                        fontsize: float):
+    """Sets 'seaborn-notebook' as plt.style and defines plot parameters
+    such as fontsize and grids. 
+    
+    :param figure_width (float): Figure width in inches
+    :param figure_height (float): Figure height in inches
+    :param fontsize (float): Font size in pt
+
+
+    """
+    mpl.rcParams.update(mpl.rcParamsDefault)
+    plt.style.use(['seaborn-notebook'])
+    plt.rcParams['axes.grid'] = True
+    plt.rcParams['grid.color'] = "grey"
+    plt.rcParams['grid.linestyle'] = "-"
+    plt.rcParams['grid.alpha'] = "0.3"
+    plt.rcParams['figure.figsize'] = (figure_width, figure_height)
+    plt.rcParams['font.size'] = fontsize
+    plt.rcParams['axes.labelsize'] = plt.rcParams['font.size']
+    plt.rcParams['axes.titlesize'] = plt.rcParams['font.size']
+    plt.rcParams['legend.fontsize'] = 0.75*plt.rcParams['font.size']
+    plt.rcParams['xtick.labelsize'] = 0.75*plt.rcParams['font.size']
+    plt.rcParams['ytick.labelsize'] = 0.75*plt.rcParams['font.size']
+    plt.rcParams['lines.linewidth']=3
+
+
