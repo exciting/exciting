66bb3825fc042ed51fcba1c90e046faa5b025c3e
Fabian Peschel <fabian.peschel@physik.hu-berlin.de>
2022-11-16 19:38:45 +0100

    Update excitingtools documentation:
    * More comprehensive README, with working examples
    * Add CONTRIBUTING and CITATION files

--- tools/exciting_tools/excitingtools/exciting_obj_parsers/__init__.py
+++ tools/exciting_tools/excitingtools/exciting_obj_parsers/__init__.py
@@ -1,2 +1,3 @@
-# from excitingtools.exciting_obj_parsers.gw_eigenvalues import gw_eigenvalue_parser, OxygenEvalQPColumns, NitrogenEvalQPColumns
+# from excitingtools.exciting_obj_parsers.gw_eigenvalues import gw_eigenvalue_parser, OxygenEvalQPColumns, \
+# NitrogenEvalQPColumns
 from excitingtools.exciting_obj_parsers.ks_band_structure import parse_band_structure
