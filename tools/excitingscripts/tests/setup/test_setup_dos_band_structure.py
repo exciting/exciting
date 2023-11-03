88b432ff3e8d3cffd0460feb7ce65ca32b69100d
Mara-Antonia Voiculescu <mara-antonia.voiculescu@physik.hu-berlin.de>
2023-11-03 22:42:38 +0100

    excitingscripts: Setup DOS and Band-Structure
    - Updates the former excitingscripts.setup.band_structure script to also set up the DOS
     calculation with standard options. The new name of the script is
     excitingscripts.setup.dos_band_structure.
    
    - Update the excitingscripts.plot.band_structure script to properly handle points in the band path
     with breakfater ="true".
    
    - Updates the Jupyter tutorial "Electronic Band-Structure and Density of States".

--- tools/excitingscripts/tests/setup/test_setup_dos_band_structure.py
+++ tools/excitingscripts/tests/setup/test_setup_dos_band_structure.py
@@ -1,5 +1,5 @@
 import pytest
-from excitingscripts.setup.band_structure import setup_band_structure
+from excitingscripts.setup.dos_band_structure import setup_dos_band_structure
 
 from excitingtools.exciting_dict_parsers.input_parser import parse_element_xml
 from excitingtools.exciting_obj_parsers.input_xml import parse_input_xml
@@ -42,8 +42,9 @@ def input_xml_mock(tmp_path) -> MockFile:
     return MockFile(input_xml_file, input_xml_str)
 
 
-def test_setup_band_structure(input_xml_mock, tmp_path):
+def test_setup_dos_band_structure(input_xml_mock, tmp_path):
     properties_ref = {
+        "dos": {"nsmdos": 2, "nwdos": 1000, "winddos": [-0.3, 0.3]},
         "bandstructure": {"plot1d": {"path": {
             "steps": 100,
             "point":
@@ -61,7 +62,7 @@ def test_setup_band_structure(input_xml_mock, tmp_path):
                     {"coord": [0.625, 0.25, 0.625], "label": "U"},
                     {"coord": [0.5, 0.0, 0.5], "label": "X"}]}}}}
 
-    setup_band_structure(input_xml_mock.full_path, root_directory=tmp_path)
+    setup_dos_band_structure(input_xml_mock.full_path, root_directory=tmp_path)
     parsed_input = parse_input_xml(tmp_path / "input.xml")
     parsed_input_properties = parsed_input.properties.to_xml()  # pylint: disable=no-member
 
