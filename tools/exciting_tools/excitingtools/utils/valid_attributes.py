04c5276af88ede3f98d15f0e42fd86b0a5fb88ca
Ronaldo Rodrigues Pela <ronaldorpela@gmail.com>
2024-03-04 09:14:46 +0100

    Update the definitions of screenshots in real-time TDDFT

--- tools/exciting_tools/excitingtools/utils/valid_attributes.py
+++ tools/exciting_tools/excitingtools/utils/valid_attributes.py
@@ -285,7 +285,14 @@ realTimeTDDFT_valid_subtrees = ['predictorCorrector', 'screenshots', 'laser']
 
 predictorCorrector_valid_attributes = [' maxIterations', 'tol'] 
 
-screenshots_valid_attributes = ['niter', 'printAbsProjCoeffs'] 
+screenshots_valid_attributes = ['niter'] 
+screenshots_valid_subtrees = ['eigenvalues', 'projectionCoefficients', 'occupations'] 
+
+eigenvalues_valid_attributes = ['nEigenvalues', 'tolerance'] 
+
+projectionCoefficients_valid_attributes = ['format', 'printAbsoluteValue'] 
+
+occupations_valid_attributes = ['format'] 
 
 laser_valid_attributes = ['fieldType'] 
 laser_valid_subtrees = ['kick', 'trapCos', 'sinSq'] 
