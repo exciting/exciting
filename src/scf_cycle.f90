bb1e28b45ec25437054d7235128748561d06dcb9
Benedikt Moritz Maurer <benedikt.moritz.maurer@physik.hu-berlin.de>
2024-04-03 10:12:50 +0200

    fix compilation

--- src/scf_cycle.f90
+++ src/scf_cycle.f90
@@ -15,6 +15,7 @@ subroutine scf_cycle(verbosity)
                              put_occ_sirius, generate_density_sirius, get_periodic_function_sirius
     use mod_potential_and_density, only: generate_density_and_magnetization
     use lo_recommendation, only: recommend_local_orbital_trial_energies
+    use trial_energy_selection, only: select_apw_trial_energies, select_local_orbital_trial_energies
 !
 
 ! !DESCRIPTION:
