31357d1e2121143d4d7d8452594f67e31df94052
Ronaldo Rodrigues Pela <ronaldorpela@gmail.com>
2024-12-23 08:58:42 +0100

    xmpi_gather

--- src/src_mpi/exciting_mpi.F90
+++ src/src_mpi/exciting_mpi.F90
@@ -5,13 +5,15 @@ module exciting_mpi
     use mod_mpi_env, only: mpiinfo
     
 #ifdef MPI 
-    use mod_mpi_bcast, only: xmpi_bcast
     use mod_mpi_allgather, only: xmpi_allgather, xmpi_allgatherv
     use mod_mpi_allreduce, only: xmpi_allreduce
+    use mod_mpi_bcast, only: xmpi_bcast
+    use mod_mpi_gather, only: xmpi_gather, xmpi_gatherv
 #else
-    use mod_serial_bcast, only: xmpi_bcast
     use mod_serial_allgather, only: xmpi_allgather, xmpi_allgatherv
     use mod_serial_allreduce, only: xmpi_allreduce
+    use mod_serial_bcast, only: xmpi_bcast
+    use mod_serial_gather, only: xmpi_gather, xmpi_gatherv
 #endif
 
     implicit none
